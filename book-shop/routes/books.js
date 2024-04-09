dc534050cf194387d0b686723220f197f653395b
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-09 21:49:48 +0900

    Refactor books routes for better readability and consistency

--- book-shop/routes/books.js
+++ book-shop/routes/books.js
@@ -4,17 +4,17 @@ const router = express.Router();
 router.use(express.json());
 
 // 전체 도서 조회
-router.get('/books', (req, res) => {
+router.get('/', (req, res) => {
     res.json('전체 도서 조회');
 });
 
 // 개별 도서 조회 
-router.get('/books/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.json('개별 도서 조회 ');
 });
 
 // 카테고리 별 도서 목록 조회 
-router.get('/books', (req, res) => {
+router.get('/', (req, res) => {
     res.json('카테고리 별 도서 목록 조회 ');
 });
 
