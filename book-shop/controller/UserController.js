cb1989837b2c8639a33263c66498a4ed3daf4126
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-10 22:13:25 +0900

    로그인 API 구현 (jwt 모듈, dotenv 모듈 추가)
    토큰 발행 후 토큰을 쿠키에 담는 작업도 추가하였음. 
    http status code는 계속 고민중 
    401 : Unauthorized (미인증) - 서버가 누군지 모름
    403 : Forbidden (접근 권리 없음) - 서버가 누군지 앎

--- book-shop/controller/UserController.js
+++ book-shop/controller/UserController.js
@@ -1,5 +1,8 @@
 const conn = require('../mariadb'); // db 모듈
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
+const jwt = require('jsonwebtoken'); // jwt 모듈 
+const dotenv = require('dotenv'); // dotenv 모듈
+dotenv.config();
 
 const join = (req, res) => {
     const {email, password} = req.body;
@@ -17,7 +20,39 @@ const join = (req, res) => {
 };
 
 const login = (req, res) => {
-    res.json('로그인');
+    const {email, password} = req.body;
+
+    let sql = `SELECT * FROM users WHERE email = ?`;
+    conn.query(sql, email,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            } 
+
+            const loginUser = results[0];
+            if (loginUser && loginUser.password == password) {
+                // 토큰 발행 
+                const token = jwt.sign({
+                    email : loginUser.email
+                }, process.env.PRIVATE_KEY, {
+                    expiresIn : '5m',
+                    issuer : "hanbirang"
+                });
+
+                //토큰 쿠키에 담기 
+                res.cookie("token", token, {
+                    httpOnly : true
+                });
+                console.log(token);
+
+                return res.status(StatusCodes.OK).json(results);
+            } else {
+                return res.status(StatusCodes.UNAUTHORIZED).end();
+                // 401 : Unauthorized (미인증) - 서버가 누군지 모름 
+                // 403 : Forbidden (접근 권리 없음) - 서버가 누군지 앎   
+            }
+        })
 };
 
 const passwordResetRequest = (req, res) => {
