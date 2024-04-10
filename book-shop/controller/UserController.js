6d090334375c7178f6f4a0a2d17534ed889a7f77
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-10 21:32:25 +0900

    Create UserController.js
    users.js의 회원가입 콜백함수를 따로 빼서 저장함.

---
+++ book-shop/controller/UserController.js
@@ -0,0 +1,19 @@
+const conn = require('../mariadb'); // db 모듈
+const {StatusCodes} = require('http-status-codes'); // status code 모듈
+
+const join = (req, res) => {
+    const {email, password} = req.body;
+
+    let sql = `INSERT INTO users (email, password) VALUES (?, ?)`;
+    let values = [email, password];
+    conn.query(sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            return res.status(StatusCodes.CREATED).json(results);
+        });
+}
+
+module.exports = join;
