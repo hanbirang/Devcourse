508e479c91d7235b29a5c2a258f0cd8eedcf2a40
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-10 21:04:03 +0900

    회원가입 API 구현 (데이터베이스와 연결)

--- book-shop/routes/users.js
+++ book-shop/routes/users.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../mariadb');
 
 router.use(express.json());
 
 // 회원 가입
 router.post('/join', (req, res) => {
-    res.json('회원가입');
+    const {email, password} = req.body;
+
+    let sql = `INSERT INTO users (email, password) VALUES (?, ?)`;
+    let values = [email, password];
+    conn.query(sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).end(); // BAD REQUEST
+            }
+            res.status(201).json(results);
+        });
 });
 
 // 로그인
