e193f1d5d8323213642a958b5568edf9f6be3311
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-11 22:04:07 +0900

    w7d4 - 전체 도서 조회 코드 안에 카테고리 별 조회 코드 넣기
    query가 있으면 카테고리 별 조회로, 없으면 전체 도서 조회 해줌

--- book-shop/controller/BookController.js
+++ book-shop/controller/BookController.js
@@ -2,15 +2,34 @@ const conn = require('../mariadb'); // db 모듈
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
 
 const allBooks = (req, res) => {
-    // (요약된) 전체 도서 리스트
-    let sql = `SELECT * FROM books`;
-    conn.query(sql, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-        return res.status(StatusCodes.OK).json(results);
-    });
+    let {category_id} = req.query;
+
+    if (category_id) {
+        // 카테고리 별 조회 
+        let sql = `SELECT * FROM books WHERE category_id = ?`;
+        conn.query(sql, category_id,
+            (err, results) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+
+                if (results.length)
+                    return res.status(StatusCodes.OK).json(results);
+                else 
+                    return res.status(StatusCodes.NOT_FOUND).end();
+        });
+    } else {
+        // (요약된) 전체 도서 리스트
+        let sql = `SELECT * FROM books`;
+        conn.query(sql, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            return res.status(StatusCodes.OK).json(results);
+        });
+    }
 };
 
 const bookDetail = (req, res) => {
@@ -32,26 +51,7 @@ const bookDetail = (req, res) => {
     });
 };
 
-const booksByCategory = (req, res) => {
-    let {category_id} = req.query;
-
-    let sql = `SELECT * FROM books WHERE category_id = ?`;
-    conn.query(sql, category_id,
-        (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-
-            if (results.length)
-                return res.status(StatusCodes.OK).json(results);
-            else 
-                return res.status(StatusCodes.NOT_FOUND).end();
-    });
-};
-
 module.exports = {
     allBooks,
-    bookDetail,
-    booksByCategory
+    bookDetail
 };
