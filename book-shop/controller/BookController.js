ababa96274c2594bf41e5d4fe4bd27915f62f02e
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-11 21:50:22 +0900

    w7d4 - 카테고리별 도서 목록 조회 API 추가
    워크벤치에서 데이터베이스에 category_id를 추가해준뒤 수정해주었음. 그러나 books.js 코드에서 전체 도서 조회와의 route가 똑같아 전체 도서 조회와 카테고리별 도서 목록 조회 둘 중 하나만 나오는 상황이 발생함. 이를 해결해야 함.

--- book-shop/controller/BookController.js
+++ book-shop/controller/BookController.js
@@ -33,7 +33,21 @@ const bookDetail = (req, res) => {
 };
 
 const booksByCategory = (req, res) => {
-    res.json('카테고리 별 도서 목록 조회 ');
+    let {category_id} = req.query;
+
+    let sql = `SELECT * FROM books WHERE category_id = ?`;
+    conn.query(sql, category_id,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            if (results.length)
+                return res.status(StatusCodes.OK).json(results);
+            else 
+                return res.status(StatusCodes.NOT_FOUND).end();
+    });
 };
 
 module.exports = {
