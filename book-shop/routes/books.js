433862a934f95cab284d235d171d0b4c57130370
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-11 16:33:45 +0900

    w7d4 - books.js에 controller에 대한 모듈 추가
    controller 폴더에 있는 BookController의 모듈과 연결해줌. 콜백함수를 BookController.js에 저장하면서, 이 파일은 라우터 역할만 하게 된다.

--- book-shop/routes/books.js
+++ book-shop/routes/books.js
@@ -1,21 +1,15 @@
 const express = require('express');
+const {
+    allBooks, 
+    bookDetail, 
+    booksByCategory 
+ } = require('../controller/BookController');
 const router = express.Router();
 
 router.use(express.json());
 
-// 전체 도서 조회
-router.get('/', (req, res) => {
-    res.json('전체 도서 조회');
-});
-
-// 개별 도서 조회 
-router.get('/:id', (req, res) => {
-    res.json('개별 도서 조회 ');
-});
-
-// 카테고리 별 도서 목록 조회 
-router.get('/', (req, res) => {
-    res.json('카테고리 별 도서 목록 조회 ');
-});
+router.get('/', allBooks); // 전체 도서 조회
+router.get('/:id', bookDetail); // 개별 도서 조회 
+router.get('/', booksByCategory); // 카테고리 별 도서 목록 조회 
 
 module.exports = router;
