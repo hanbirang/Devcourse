e5dbfc2387f3dafc24163cf1e2ac7caefa4ee807
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-12 16:56:27 +0900

    w7d5 - 카테고리 별 도서 조회, 신간 도서 목록 조회 추가
    DATE_SUB을 이용하여 현재 날짜를 기준으로 한 달 이내의 책인 경우 신간으로 구분하여 보여줌. if 문을 통해 sql문을 설정함으로써, 코드의 중복을 줄임

--- book-shop/controller/BookController.js
+++ book-shop/controller/BookController.js
@@ -1,35 +1,37 @@
 const conn = require('../mariadb'); // db 모듈
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
 
+// (카테고리 별, 신간 여부) 전체 도서 목록 조회 
 const allBooks = (req, res) => {
-    let {category_id} = req.query;
+    let {category_id, newBook} = req.query;
 
-    if (category_id) {
-        // 카테고리 별 조회 
-        let sql = `SELECT * FROM books WHERE category_id = ?`;
-        conn.query(sql, category_id,
-            (err, results) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
-
-                if (results.length)
-                    return res.status(StatusCodes.OK).json(results);
-                else 
-                    return res.status(StatusCodes.NOT_FOUND).end();
-        });
-    } else {
-        // (요약된) 전체 도서 리스트
-        let sql = `SELECT * FROM books`;
-        conn.query(sql, (err, results) => {
+    let sql = `SELECT * FROM books`;
+    let values = [];
+    if (category_id && newBook) {
+        sql += ` WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();`;
+        values = [category_id, newBook];
+    }
+    else if (category_id) {
+        sql += ` WHERE category_id = ?`;
+        values = category_id;
+    }
+    else if (newBook) {
+        sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();`;
+        values = news;
+    }
+    
+    conn.query(sql, values,
+        (err, results) => {
             if (err) {
                 console.log(err);
                 return res.status(StatusCodes.BAD_REQUEST).end();
             }
-            return res.status(StatusCodes.OK).json(results);
-        });
-    }
+
+            if (results.length)
+                return res.status(StatusCodes.OK).json(results);
+            else 
+                return res.status(StatusCodes.NOT_FOUND).end();
+    });
 };
 
 const bookDetail = (req, res) => {
@@ -51,6 +53,7 @@ const bookDetail = (req, res) => {
                 return res.status(StatusCodes.NOT_FOUND).end();
     });
 };
+
 
 module.exports = {
     allBooks,
