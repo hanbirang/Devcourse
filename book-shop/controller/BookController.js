44f741f82c117e096bfbe7bfdef63b8a6e54d136
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-15 21:16:06 +0900

    w8d1 - 도서 개별 조회 좋아요 여부 추가 구현

--- book-shop/controller/BookController.js
+++ book-shop/controller/BookController.js
@@ -43,12 +43,18 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-    let {id} = req.params;
-    id = parseInt(id);
+    let {user_id} = req.body;
+    let book_id = req.params.id;
 
-    let sql = `SELECT * FROM books LEFT JOIN category
-            ON books.category_id = category.id WHERE books.id = ?;`;
-    conn.query(sql, id,
+    let sql = `SELECT *,
+	                    (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes,
+	                    (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked
+                    FROM books
+                    LEFT JOIN category
+	                ON books.category_id = category.category_id
+                    WHERE books.id = ?;`;
+    let values = [user_id, book_id, book_id];
+    conn.query(sql, values,
         (err, results) => {
             if (err) {
                 console.log(err);
