9b5e28989f654b754354f64a40dd611863c3b207
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-16 19:28:42 +0900

    w8d2 - 장바구니 API 관련 sql 추가

--- book-shop/data.sql
+++ book-shop/data.sql
@@ -49,7 +49,7 @@ INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(2, 3);
 INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(2, 5);
 
 //* 좋아요 삭제 */
-DELETE FROM likes WHERE user_id = 1 AND liked_book_id = 3;
+DELETE FROM Bookshop.likes WHERE user_id = 1 AND liked_book_id = 3;
 
 /* 조건을 만족하는 행 개수 */
 SELECT count(*) FROM likes WHERE liked_book_id = 1;
@@ -60,6 +60,21 @@ SELECT *,
     FROM books;
 
 /* 개별 도서 조회 시, 사용자가 좋아요를 했는지 여부를 포함 */
-SELECT EXISTS (SELECT * FROM likes WHERE user_id=1 AND liked_book_id=1)
+SELECT EXISTS (SELECT * FROM Bookshop.likes WHERE user_id=1 AND liked_book_id=1)
 
 SELECT count(*) FROM likes WHERE user_id=1 AND liked_book_id = 1;
+
+/* 장바구니 담기 */
+INSERT INTO cartItems (book_id, quantity, user_id) VALUES(1, 1, 1);
+INSERT INTO cartItems (book_id, quantity, user_id) VALUES(1, 1, 1);
+
+/* 장바구니 아이템 목록 조회 */
+SELECT cartItems.id, book_id, title, summary, quantity, price
+FROM cartItems LEFT JOIN books
+ON cartItems.book_id = books.id;
+
+/* 장바구니 아이템 삭제 */
+DELETE FROM cartItems WHERE id = ?;
+
+/* 장바구니에서 선택한(장바구니 도서 id) 아이템 목록 조회 (=선택한 장바구니 상품 목록 조회) */
+SELECT * FROM cartItems  WHERE user_id = 1 AND id IN (1, 3);
