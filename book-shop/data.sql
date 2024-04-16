26a0884903f040cf9bdab920419c414f85cf6a64
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-16 16:29:15 +0900

    w8d1 - 좋아요 API 관련 sql 추가

--- book-shop/data.sql
+++ book-shop/data.sql
@@ -31,9 +31,35 @@ VALUES ("효녀 심청", 12, 0, "종이책", 9, "심청아..", "공양미 삼백
 INSERT INTO Bookshop.books (title, img, category_id, form, isbn, summary, detail, author, pages, contents, price, pub_date)
 VALUES ("혹부리 영감", 22, 2, "ebook", 10, "노래 주머니..", "혹 두개 되버림..", "김영감", 100, "목차입니다.", 20000, "2023-06-05");
 
-SELECT * FROM Bookshop.books LEFT
-JOIN Bookshop.category ON books.category_id = category.id;
+SELECT * FROM Bookshop.books LEFT JOIN Bookshop.category ON books.category_id = category.id;
 
 SELECT * FROM Bookshop.books LEFT
 JOIN Bookshop.category ON books.category_id = category.id
 WHERE books.id = 1;
+
+/* 좋아요 추가 */
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(1, 1);
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(1, 2);
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(1, 3);
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(3, 1);
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(4, 4);
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(2, 1);
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(2, 2);
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(2, 3);
+INSERT INTO Bookshop.likes(user_id, liked_book_id) VALUES(2, 5);
+
+//* 좋아요 삭제 */
+DELETE FROM likes WHERE user_id = 1 AND liked_book_id = 3;
+
+/* 조건을 만족하는 행 개수 */
+SELECT count(*) FROM likes WHERE liked_book_id = 1;
+
+/* 좋아요 개수가 포함된 books 테이블 조회 */
+SELECT *,
+    (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes
+    FROM books;
+
+/* 개별 도서 조회 시, 사용자가 좋아요를 했는지 여부를 포함 */
+SELECT EXISTS (SELECT * FROM likes WHERE user_id=1 AND liked_book_id=1)
+
+SELECT count(*) FROM likes WHERE user_id=1 AND liked_book_id = 1;
