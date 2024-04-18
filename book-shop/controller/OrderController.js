cec04ed131c6c4bd3fe50b4a617de289a9ffcbbc
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-18 14:28:22 +0900

    w8d3 - 주문하기 API 구현 (orders 테이블에 추가)
    이전에는 delivery table에 추가하는 API를 구현하였고, 이번에는 orders 테이블에 추가하고 있음

--- book-shop/controller/OrderController.js
+++ book-shop/controller/OrderController.js
@@ -2,12 +2,27 @@ const conn = require('../mariadb'); // db 모듈
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
 
 const order = (req, res) => {
-    const {items, delivery, totalQuantity, totalPrice, userId} = req.body;
+    const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
 
-    let delivery_id;
+    let delivery_id = 3;
+    let order_id;
 
     let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
     let values = [delivery.address, delivery.receiver, delivery.contact];
+    // conn.query(sql, values,
+    //     (err, results) => {
+    //         if (err) {
+    //             console.log(err);
+    //             return res.status(StatusCodes.BAD_REQUEST).end();
+    //         }
+
+    //         delivery_id = results.insertId;
+            
+    //         return res.status(StatusCodes.OK).json(results);
+    // });
+    sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+    VALUES (?, ?, ?, ?, ?);`
+    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
     conn.query(sql, values,
         (err, results) => {
             if (err) {
@@ -15,7 +30,8 @@ const order = (req, res) => {
                 return res.status(StatusCodes.BAD_REQUEST).end();
             }
 
-            delivery_id = results.insertId;
+            order_id = results.insertId;
+            console.log(order_id);
             
             return res.status(StatusCodes.OK).json(results);
     });
