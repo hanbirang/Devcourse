f36a2c44c5b601a69dd4a84d8cdc27f1998b24e4
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-18 14:56:16 +0900

    w8d3 - 주문하기 API 구현 (orderedBook에 데이터 삽입)
    한 번에 여러 개의 데이터를 insert 하는 방식을 사용하여, orderedBook 테이블에 데이터를 여러 개 추가한다.

--- book-shop/controller/OrderController.js
+++ book-shop/controller/OrderController.js
@@ -5,7 +5,7 @@ const order = (req, res) => {
     const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
 
     let delivery_id = 3;
-    let order_id;
+    let order_id = 3;
 
     let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
     let values = [delivery.address, delivery.receiver, delivery.contact];
@@ -21,17 +21,34 @@ const order = (req, res) => {
     //         return res.status(StatusCodes.OK).json(results);
     // });
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
-    VALUES (?, ?, ?, ?, ?);`
+            VALUES (?, ?, ?, ?, ?);`
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    conn.query(sql, values,
+    // conn.query(sql, values,
+    //     (err, results) => {
+    //         if (err) {
+    //             console.log(err);
+    //             return res.status(StatusCodes.BAD_REQUEST).end();
+    //         }
+
+    //         order_id = results.insertId;
+    //         console.log(order_id);
+            
+    //         return res.status(StatusCodes.OK).json(results);
+    // });
+
+    sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`
+
+    // imtes.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) > 
+    values = [];
+    items.forEach((item) => {
+        values.push([order_id, item.book_id, item.quantity]);
+    })
+    conn.query(sql, [values],
         (err, results) => {
             if (err) {
                 console.log(err);
                 return res.status(StatusCodes.BAD_REQUEST).end();
             }
-
-            order_id = results.insertId;
-            console.log(order_id);
             
             return res.status(StatusCodes.OK).json(results);
     });
