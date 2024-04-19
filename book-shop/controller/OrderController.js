248d25e9c576863560b1a4fbee6381bcb058064d
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-19 22:01:16 +0900

    w8d4 - 주문하면 cartItems 아이템 삭제하는 API

--- book-shop/controller/OrderController.js
+++ book-shop/controller/OrderController.js
@@ -13,33 +13,43 @@ const order = async (req, res) => {
 
     const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
 
-    // delivery 테이블 삽입 
-    let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
-    let values = [delivery.address, delivery.receiver, delivery.contact];
-    let [results] = await conn.execute(sql, values);
+    // // delivery 테이블 삽입 
+    // let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
+    // let values = [delivery.address, delivery.receiver, delivery.contact];
+    // let [results] = await conn.execute(sql, values);
 
-    let delivery_id = results.insertId;
+    // let delivery_id = results.insertId;
 
-    // orders 테이블 삽입 
-    sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
-            VALUES (?, ?, ?, ?, ?);`
-    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    [results] = await conn.execute(sql, values);
-    let order_id = results.insertId;
+    // // orders 테이블 삽입 
+    // sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+    //         VALUES (?, ?, ?, ?, ?);`
+    // values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+    // [results] = await conn.execute(sql, values);
+    // let order_id = results.insertId;
 
-    // orderedBook 테이블 삽입 
-    sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`
+    // // orderedBook 테이블 삽입 
+    // sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`
 
-    // imtes.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) > 
-    values = [];
-    items.forEach((item) => {
-        values.push([order_id, item.book_id, item.quantity]);
-    })
+    // // imtes.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) > 
+    // values = [];
+    // items.forEach((item) => {
+    //     values.push([order_id, item.book_id, item.quantity]);
+    // })
 
-    results = await conn.query(sql, [values]);
+    // results = await conn.query(sql, [values]);
 
-    return res.status(StatusCodes.OK).json(results[0]);
+    let result = await deleteCartItems(conn);
+
+    return res.status(StatusCodes.OK).json(result);
 };
+
+const deleteCartItems = async (conn) => {
+    let sql = `DELETE FROM cartItems WHERE id IN (?);`;
+    let values = [1, 2, 3];
+
+    let result = await conn.query(sql, [values]);
+    return result; 
+}
 
 const getOrders = (req, res) => {
     res.json('주문 목록 조회');
