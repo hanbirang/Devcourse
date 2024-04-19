5c7a93370e6965f493dad6e5995be89594e59c6f
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-19 23:03:05 +0900

    w8d5 - 주문하기 API 끝
    주문하고, 장바구니 테이블에서 삭제하는 것 까지 구현

--- book-shop/controller/OrderController.js
+++ book-shop/controller/OrderController.js
@@ -13,41 +13,45 @@ const order = async (req, res) => {
 
     const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
 
-    // // delivery 테이블 삽입 
-    // let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
-    // let values = [delivery.address, delivery.receiver, delivery.contact];
-    // let [results] = await conn.execute(sql, values);
+    // delivery 테이블 삽입 
+    let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
+    let values = [delivery.address, delivery.receiver, delivery.contact];
+    let [results] = await conn.execute(sql, values);
 
-    // let delivery_id = results.insertId;
+    let delivery_id = results.insertId;
 
-    // // orders 테이블 삽입 
-    // sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
-    //         VALUES (?, ?, ?, ?, ?);`
-    // values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    // [results] = await conn.execute(sql, values);
-    // let order_id = results.insertId;
+    // orders 테이블 삽입 
+    sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+            VALUES (?, ?, ?, ?, ?);`
+    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+    [results] = await conn.execute(sql, values);
+    let order_id = results.insertId;
 
-    // // orderedBook 테이블 삽입 
-    // sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`
 
-    // // imtes.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) > 
-    // values = [];
-    // items.forEach((item) => {
-    //     values.push([order_id, item.book_id, item.quantity]);
-    // })
+    // items를 가지고, 장바구니에서 book_id, quantity 조회 
+    sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`;
+    let [orderItems, fields] = await conn.query(sql, [items]);
 
-    // results = await conn.query(sql, [values]);
+    // orderedBook 테이블 삽입 
+    sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`
 
-    let result = await deleteCartItems(conn);
+    // imtes.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) > 
+    values = [];
+    orderItems.forEach((item) => {
+        values.push([order_id, item.book_id, item.quantity]);
+    })
+
+    results = await conn.query(sql, [values]);
+
+    let result = await deleteCartItems(conn, items);
 
     return res.status(StatusCodes.OK).json(result);
 };
 
-const deleteCartItems = async (conn) => {
+const deleteCartItems = async (conn, items) => {
     let sql = `DELETE FROM cartItems WHERE id IN (?);`;
-    let values = [1, 2, 3];
 
-    let result = await conn.query(sql, [values]);
+    let result = await conn.query(sql, [items]);
     return result; 
 }
 
