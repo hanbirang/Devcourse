04874d46ffa9c2834667b018b11ce968ab87056a
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-19 20:40:10 +0900

    w8d4 - 주문하기 API 수정
    비동기 처리를 위해 async await을 사용하였으며, conn.query 대신 execute를 사용함. 또한 마지막 conn.query에서 오류가 발생하여, results의 0번째 정보만 빼서 respond하게 함. delivery 테이블에 삽입하여 delivery_id를 가져와 다음 sql에 사용함 (orders 테이블에 삽입하기 위해) orders 테이블에 삽입하고 order_id를 가져와 다음 sql에 사용함 (orderedBook 테이블에 삽입하기 위해) orderedBook 테이블에 데이터 삽입함.

--- book-shop/controller/OrderController.js
+++ book-shop/controller/OrderController.js
@@ -1,41 +1,33 @@
-const conn = require('../mariadb'); // db 모듈
+// const conn = require('../mariadb'); // db 모듈
+const mariadb = require('mysql2/promise');
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
 
-const order = (req, res) => {
+const order = async (req, res) => {
+    const conn = await mariadb.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root', 
+        database: 'Bookshop',
+        dateStrings: true
+    });
+
     const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
 
-    let delivery_id = 3;
-    let order_id = 3;
-
+    // delivery 테이블 삽입 
     let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
     let values = [delivery.address, delivery.receiver, delivery.contact];
-    // conn.query(sql, values,
-    //     (err, results) => {
-    //         if (err) {
-    //             console.log(err);
-    //             return res.status(StatusCodes.BAD_REQUEST).end();
-    //         }
+    let [results] = await conn.execute(sql, values);
 
-    //         delivery_id = results.insertId;
-            
-    //         return res.status(StatusCodes.OK).json(results);
-    // });
+    let delivery_id = results.insertId;
+
+    // orders 테이블 삽입 
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
             VALUES (?, ?, ?, ?, ?);`
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    // conn.query(sql, values,
-    //     (err, results) => {
-    //         if (err) {
-    //             console.log(err);
-    //             return res.status(StatusCodes.BAD_REQUEST).end();
-    //         }
+    [results] = await conn.execute(sql, values);
+    let order_id = results.insertId;
 
-    //         order_id = results.insertId;
-    //         console.log(order_id);
-            
-    //         return res.status(StatusCodes.OK).json(results);
-    // });
-
+    // orderedBook 테이블 삽입 
     sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`
 
     // imtes.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) > 
@@ -43,15 +35,10 @@ const order = (req, res) => {
     items.forEach((item) => {
         values.push([order_id, item.book_id, item.quantity]);
     })
-    conn.query(sql, [values],
-        (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            
-            return res.status(StatusCodes.OK).json(results);
-    });
+
+    results = await conn.query(sql, [values]);
+
+    return res.status(StatusCodes.OK).json(results[0]);
 };
 
 const getOrders = (req, res) => {
