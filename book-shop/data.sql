1e3fc2abbd256570da3fc29d045e6af2aeeca962
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-19 23:09:43 +0900

    w8d4 & w8d5 - 주문하기 관련 sql 추가

--- book-shop/data.sql
+++ book-shop/data.sql
@@ -78,3 +78,42 @@ DELETE FROM cartItems WHERE id = ?;
 
 /* 장바구니에서 선택한(장바구니 도서 id) 아이템 목록 조회 (=선택한 장바구니 상품 목록 조회) */
 SELECT * FROM cartItems  WHERE user_id = 1 AND id IN (1, 3);
+
+/* 주문하기 */
+/* 배송 정보 입력 */
+INSERT INTO delivery (address, receiver, contact) VALUES ("서울시 강동구", "임한비", "010-1234-5678");
+INSERT INTO delivery (address, receiver, contact) VALUES ("서울시 성동구", "림한비", "010-2222-2222");
+
+/* 주문 정보 입력 */
+INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+VALUES ("어린왕자들", 3, 60000, 1, 1);
+
+/* 주문  상세 목록 입력 */
+INSERT INTO orderedBook (order_id, book_id, quantity)
+VALUES (1, 1, 1);
+INSERT INTO orderedBook (order_id, book_id, quantity)
+VALUES (1, 3, 2);
+
+/* 주문하기 */
+/* 배송 정보 입력 */
+INSERT INTO delivery (address, receiver, contact) VALUES ("서울시 강동구", "임한비", "010-1234-5678");
+INSERT INTO delivery (address, receiver, contact) VALUES ("서울시 성동구", "림한비", "010-2222-2222");
+const delivery_id = SELECT max(id) FROM delivery;
+
+/* 주문 정보 입력 */
+INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+VALUES ("어린왕자들", 3, 60000, 1, delivery_id);
+const order_id = SELECT max(id) FROM delivery;
+
+
+/* 주문  상세 목록 입력 */
+INSERT INTO orderedBook (order_id, book_id, quantity)
+VALUES (order_id, 1, 1);
+INSERT INTO orderedBook (order_id, book_id, quantity)
+VALUES (order_id, 3, 2);
+
+SELECT max(id) FROM orderedBook;
+SELECT last_insert_id();
+
+/* 장바구니 삭제 */
+DELETE FROM cartItems WHERE id IN (1, 2, 3);
