f661e801cf175dacad6612efc1199fcf8ec1104c
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-19 16:44:05 +0900

    w8d4 - 비동기 처리를 위한 mariadb.js 수정

--- book-shop/mariadb.js
+++ book-shop/mariadb.js
@@ -1,11 +1,17 @@
-const mariadb = require('mysql2');
 
-const connection = mariadb.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root', 
-    database: 'Bookshop',
-    dateStrings: true
-});
+// mysql 모듈 소환
+const mariadb = require('mysql2/promise');
 
+// DB와 연결 통로 생성 
+const connection = async () => {
+    const conn = await mariadb.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root', 
+        database: 'Bookshop',
+        dateStrings: true
+    });
+
+    return conn;
+}
 module.exports = connection;
