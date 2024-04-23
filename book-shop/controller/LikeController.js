c54cda7c82d791d6280b71dec845ed05a00368b7
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-23 14:49:17 +0900

    [w9d2] 좋아요 API 회원 인증 모듈화

--- book-shop/controller/LikeController.js
+++ book-shop/controller/LikeController.js
@@ -1,3 +1,4 @@
+const ensureAuthorization = require('../auth') // 인증 모듈 
 const jwt = require('jsonwebtoken');
 const conn = require('../mariadb'); // db 모듈
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
@@ -57,23 +58,6 @@ const removeLike = (req, res) => {
         });
     }
 };
-
-function ensureAuthorization(req, res) {
-    try {
-        let receivedJwt = req.headers["authorization"];
-        console.log("received jwt : ", receivedJwt);
-
-        let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
-        console.log(decodedJwt);
-
-        return decodedJwt;
-    } catch (err) {
-        console.log(err.name);
-        console.log(err.message);
-
-        return err;
-    }
-}
 
 module.exports = {
     addLike,
