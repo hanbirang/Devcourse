948097bbb4006a7e24de44c4bc6e4820e473cca4
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-24 23:12:16 +0900

    [ w9d3 ]  faker 이용해서 가짜 사용자 1명 만들어주는 API 만들어보기

--- random-project/random-user.js
+++ random-project/random-user.js
@@ -1,5 +1,19 @@
+const express = require('express');
+const app = express();
+
 const { faker } = require('@faker-js/faker');
 
-console.log(faker.internet.userName());
-console.log(faker.internet.email());
-console.log(faker.internet.password());
+// localhost:2424/fake/users
+// 한 명의 사용자 정보 생성
+// email, password, fullName, contact 
+
+app.get('/fake/users', function (req, res) {
+    res.status(200).json({
+        email : faker.internet.email(),
+        password : faker.internet.password(),
+        fullName : faker.person.fullName(),
+        number : faker.phone.number()
+    });
+})
+
+app.listen(2424);
