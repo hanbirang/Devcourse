3a0b49af201a50214fcddf5ea684e7a25b2bc711
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-25 11:43:14 +0900

    [ w9d3 ] books.js에 유효성 검사 구현

--- book-shop/routes/books.js
+++ book-shop/routes/books.js
@@ -4,10 +4,33 @@ const {
     bookDetail
  } = require('../controller/BookController');
 const router = express.Router();
+const {param, query, validationResult} = require('express-validator');
+
+const validate = (req, res, next) => {
+  const err = validationResult(req)
+
+  if (err.isEmpty()) {
+      return next(); // 다음 할 일 (미들 웨어, 함수)
+  } else {
+      return res.status(400).json(err.array());
+  }
+};
 
 router.use(express.json());
 
-router.get('/', allBooks); // 전체 도서 조회 & 카테고리 별 도서 조회 
-router.get('/:id', bookDetail); // 개별 도서 조회 
+router.get(
+    '/',
+    [
+        query('limit').notEmpty().isInt().withMessage('page 당 도서 수 확인 필요'),
+        query('currentPage').notEmpty().isInt().withMessage('현재 페이지 확인 필요'),
+        validate
+    ], allBooks); // 전체 도서 조회 & 카테고리 별 도서 조회 
+
+router.get(
+    '/:id',
+    [
+        param('id').notEmpty().isInt().withMessage('book id 필요'),
+        validate
+    ], bookDetail); // 개별 도서 조회 
 
 module.exports = router;
