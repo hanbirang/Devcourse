159357de3d7b897077097ec32fb5f3fa01883909
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-25 09:06:07 +0900

    [ w9d3 ] 원하는 사용자 수만큼 가짜 데이터를 만들어주는 API
    쿼리로 사용자 수를 받는다

--- random-project/random-user.js
+++ random-project/random-user.js
@@ -7,13 +7,21 @@ const { faker } = require('@faker-js/faker');
 // 한 명의 사용자 정보 생성
 // email, password, fullName, contact 
 
-app.get('/fake/users', function (req, res) {
-    res.status(200).json({
-        email : faker.internet.email(),
-        password : faker.internet.password(),
-        fullName : faker.person.fullName(),
-        number : faker.phone.number()
-    });
+app.get('/fake/users/', function (req, res) {
+    const {num} = req.query;
+    
+    let index = 1;
+    let users = [];
+    while (index <= num) {
+        users.push({
+            email : faker.internet.email(),
+            password : faker.internet.password(),
+            fullName : faker.person.fullName(),
+            number : faker.phone.number()
+        });
+        index++;
+    }
+    res.status(200).json(users);
 })
 
 app.listen(2424);
