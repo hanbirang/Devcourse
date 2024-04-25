79c9a4905f50fa42168405531237f9cfd3a4090f
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-04-25 11:44:25 +0900

    [ w9d3 ] likes.js에 유효성 검사 구현

--- book-shop/routes/likes.js
+++ book-shop/routes/likes.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const{addLike, removeLike} = require('../controller/LikeController');
+const {body, param, validationResult} = require('express-validator');
+
+const validate = (req, res, next) => {
+  const err = validationResult(req)
+
+  if (err.isEmpty()) {
+      return next(); // 다음 할 일 (미들 웨어, 함수)
+  } else {
+      return res.status(400).json(err.array());
+  }
+};
 
 router.use(express.json());
 
-router.post('/:id', addLike); // 좋아요 추가
-router.delete('/:id', removeLike); // 좋아요 삭제 
+router.post(
+    '/:id',
+    [
+        param('id').notEmpty().isInt().withMessage('book id 필요'),
+        validate
+    ], addLike); // 좋아요 추가
+
+router.delete(
+    '/:id',
+    [
+        param('id').notEmpty().isInt().withMessage('book id 필요'),
+        validate
+    ], removeLike); // 좋아요 삭제 
 
 module.exports = router;
