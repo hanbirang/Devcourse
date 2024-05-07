665fdfabb5c1e4a45c156637a4439cb2fb9171df
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-07 22:34:55 +0900

    [ w11d2 ] object 형태에서의 타입 추론 컴파일

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -1,3 +1,10 @@
-var myname = 'lhb';
-myname = 'lim'; // 타입 추론
-// myname = 1; 에러 발생 
+var student = {
+  name : 'hanbirang',
+  course : 'typescript',
+  score : 100,
+  grade : function() {
+    console.log('A');
+  }
+};
+student.name = 'lim';
+student.name = 1;
