a34cde6b7dc8643de00da7bfebe2f5365c2a8cf0
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-07 22:37:29 +0900

    [ w11d2 ] 변수와 함수의 데이터 타입 명시

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,11 +1,26 @@
-let student = {
-  name : 'hanbirang',
-  course : 'typescript',
-  score : 100,
-  grade : function() {
-    console.log('A');
-  }
-};
+// 변수의 데이터 타입 명시
+let stdId : number = 625;
+let stdName : string = 'lhb';
+let age : number = 21;
+let gender : string = 'female';
+let course : string = 'TypeScript';
+let completed : boolean = false;
 
-student.name = 'lim';
-// student.name = 1; 에러 발생 
+function getInfo(id : number) : {
+    stdId : number;
+    stdName : string;
+    age : number;  
+    gender : string; 
+    course : string;
+    completed : boolean;
+}
+{
+    return null;
+}
+// 반환되는 객체의 구조를 타입으로 지정 
+
+// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+// function Plus(a : number, b : number) : number {
+//     return a + b;
+// }
+// 만약 반환 값이 없으면 void 사용 
