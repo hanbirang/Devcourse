be1ca447de36a59ee89ff66dec8377128ecad022
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-07 22:37:56 +0900

    [ w11d2 ] 변수와 함수의 데이터 타입 명시 컴파일

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -1,10 +1,16 @@
-var student = {
-  name : 'hanbirang',
-  course : 'typescript',
-  score : 100,
-  grade : function() {
-    console.log('A');
-  }
-};
-student.name = 'lim';
-// student.name = 1; 에러 발생 
+// 변수의 데이터 타입 명시
+var stdId = 625;
+var stdName = 'lhb';
+var age = 21;
+var gender = 'female';
+var course = 'TypeScript';
+var completed = false;
+function getInfo(id) {
+    return null;
+}
+// 반환되는 객체의 구조를 타입으로 지정 
+// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+// function Plus(a : number, b : number) : number {
+//     return a + b;
+// }
+// 만약 반환 값이 없으면 void 사용 
