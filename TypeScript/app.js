65e5af35a9e80fe3df11a5c1bad2c29c613c602f
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-07 23:10:29 +0900

    [ w11d2 ] 인터페이스에 함수 추가 컴파일
    타입스크립트에서 작성한 인터페이스는 자바스크립트에서 보이지 않는다.

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -5,16 +5,34 @@ var age = 21;
 var gender = 'female';
 var course = 'TypeScript';
 var completed = false;
-function getInfo(id) {
-    return {
-        stdId: id,
-        stdName: 'lim',
-        // age : 23,
-        gender: 'female',
-        course: 'JavaScript',
-        completed: true
+var MyStudent = /** @class */ (function () {
+    function MyStudent() {
+        this.stdId = 960615;
+        this.stdName = 'hoshi';
+        this.age = 29;
+        this.gender = 'male';
+        this.course = 'node.js';
+        this.completed = true;
+    }
+    MyStudent.prototype.setName = function (name) {
+        this.stdName = name;
+        console.log('이름 설정 : ' + this.stdName);
     };
-}
+    return MyStudent;
+}());
+var myInstance = new MyStudent();
+myInstance.setName('호시');
+// function getInfo(id : number) : Student
+// {
+//     return {
+//         stdId : id,
+//         stdName : 'lim',
+//         // age : 23,
+//         gender : 'female', 
+//         course : 'JavaScript',
+//         completed : true
+//     };
+// }
 var std = {
     stdId: 960615,
     stdName: 'hoshi',
@@ -26,7 +44,7 @@ var std = {
 function setInfo(student) {
     console.log(student);
 }
-setInfo(std);
+// setInfo(std);
 // console.log(getInfo(1111));
 // 함수의 데이터 타입 명시 (매개변수, 리턴타입)
 // function Plus(a : number, b? : number) : number {
