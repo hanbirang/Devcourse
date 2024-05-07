d1ee1e8b92884823efbbfbc079fba85982ea0922
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-07 22:47:52 +0900

    [ w11d2 ] optional 기호를 사용하여 선택적 프로퍼티 만들기 컴파일

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -9,7 +9,7 @@ function getInfo(id) {
     return {
         stdId: id,
         stdName: 'lim',
-        age: 23,
+        // age : 23,
         gender: 'female',
         course: 'JavaScript',
         completed: true
@@ -17,7 +17,7 @@ function getInfo(id) {
 }
 console.log(getInfo(1111));
 // 함수의 데이터 타입 명시 (매개변수, 리턴타입)
-// function Plus(a : number, b : number) : number {
+// function Plus(a : number, b? : number) : number {
 //     return a + b;
 // }
 // 만약 반환 값이 없으면 void 사용 
