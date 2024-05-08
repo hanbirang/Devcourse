8c2b1e3a5bc3925f24a724a031bc8d89e4975ada
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-08 23:47:28 +0900

    [ w11d3 ] 생성자 컴파일

--- TypeScript/oop.js
+++ TypeScript/oop.js
@@ -1,16 +1,16 @@
 // 멤버 변수 == 속성 == 프로퍼티
 // 멤버 함수 == 메소드 
 var Employee = /** @class */ (function () {
-    function Employee() {
+    function Employee(empName, age, empJob) {
         var _this = this;
         this.printEmp = function () {
             console.log("".concat(_this.empName, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this.empJob, "\uC785\uB2C8\uB2E4."));
         };
+        this.empName = empName;
+        this.age = age;
+        this.empJob = empJob;
     }
     return Employee;
 }());
-var employee1 = new Employee();
-employee1.empName = 'lim';
-employee1.age = 21;
-employee1.empJob = 'developer';
+var employee1 = new Employee('lim', 21, 'developer');
 employee1.printEmp();
