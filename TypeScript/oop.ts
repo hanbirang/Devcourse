b0e936b4f1288b598605c0781c67d478a25bbd8f
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-08 23:33:26 +0900

    [ w11d3 ] 클래스와 객체 만들기

--- TypeScript/oop.ts
+++ TypeScript/oop.ts
@@ -1,10 +1,19 @@
-// 일반적인 직원 정보
-let empName : string;
-let age : number;
-let empJob : string;
+// 멤버 변수 == 속성 == 프로퍼티
+// 멤버 함수 == 메소드 
 
-function printEmp(empName : string, age : number, empJob : string) : void {
-    console.log(`${empName}의 나이는 ${age}이고, 직업은 ${empJob}입니다.`);
+class Employee {
+    empName : string;
+    age : number;
+    empJob : string;
+
+    printEmp = () : void => {
+        console.log(`${this.empName}의 나이는 ${this.age}이고, 직업은 ${this.empJob}입니다.`);
+    }
 }
 
-printEmp('lim', 21, 'developer');
+let employee1 = new Employee();
+employee1.empName = 'lim';
+employee1.age = 21;
+employee1.empJob = 'developer';
+
+employee1.printEmp();
