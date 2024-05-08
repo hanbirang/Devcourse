0def7a4561141b8ba5eaee8949debee01135e10a
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-08 23:47:02 +0900

    [ w11d3 ] 생성자

--- TypeScript/oop.ts
+++ TypeScript/oop.ts
@@ -6,14 +6,16 @@ class Employee {
     age : number;
     empJob : string;
 
+    constructor(empName : string, age? : number, empJob? : string) {
+        this.empName = empName;
+        this.age = age;
+        this.empJob = empJob;
+    }
+
     printEmp = () : void => {
         console.log(`${this.empName}의 나이는 ${this.age}이고, 직업은 ${this.empJob}입니다.`);
     }
 }
 
-let employee1 = new Employee();
-employee1.empName = 'lim';
-employee1.age = 21;
-employee1.empJob = 'developer';
-
+let employee1 = new Employee('lim', 21, 'developer');
 employee1.printEmp();
