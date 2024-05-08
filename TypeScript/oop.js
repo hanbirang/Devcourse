0d0dc2d2acabdd7584e01582b63b5ba7ffe88818
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-08 23:33:56 +0900

    [ w11d3 ] 클래스와 객체 만들기 컴파일

--- TypeScript/oop.js
+++ TypeScript/oop.js
@@ -1,8 +1,16 @@
-// 일반적인 직원 정보
-var empName;
-var age;
-var empJob;
-function printEmp(empName, age, empJob) {
-    console.log("".concat(empName, "\uC758 \uB098\uC774\uB294 ").concat(age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(empJob, "\uC785\uB2C8\uB2E4."));
-}
-printEmp('lim', 21, 'developer');
+// 멤버 변수 == 속성 == 프로퍼티
+// 멤버 함수 == 메소드 
+var Employee = /** @class */ (function () {
+    function Employee() {
+        var _this = this;
+        this.printEmp = function () {
+            console.log("".concat(_this.empName, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this.empJob, "\uC785\uB2C8\uB2E4."));
+        };
+    }
+    return Employee;
+}());
+var employee1 = new Employee();
+employee1.empName = 'lim';
+employee1.age = 21;
+employee1.empJob = 'developer';
+employee1.printEmp();
