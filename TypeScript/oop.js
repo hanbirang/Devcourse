ce2eb89d258d599559286e213d66cfd9b116bcba
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-08 22:30:37 +0900

    [ w11d3 ] oop.ts 컴파일

---
+++ TypeScript/oop.js
@@ -0,0 +1,8 @@
+// 일반적인 직원 정보
+var empName;
+var age;
+var empJob;
+function printEmp(empName, age, empJob) {
+    console.log("".concat(empName, "\uC758 \uB098\uC774\uB294 ").concat(age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(empJob, "\uC785\uB2C8\uB2E4."));
+}
+printEmp('lim', 21, 'developer');
