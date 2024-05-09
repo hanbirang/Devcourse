f5661bb7c4b53e51ef6e46f59f3704546c2f9ff4
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-09 10:23:37 +0900

    [ w11d3 ] 변수 선언과 생성자 합치기

--- TypeScript/oop.ts
+++ TypeScript/oop.ts
@@ -1,17 +1,11 @@
-// 멤버 변수 == 속성 == 프로퍼티
-// 멤버 함수 == 메소드 
-
 class Employee {
-    private _empName : string;
-    private _age : number;
-    private _empJob : string;
+    constructor(
+        private _empName : string,
+        private _age : number,
+        private _empJob : string
+    ) {
 
-    constructor(empName : string, age : number, empJob : string) {
-        this._empName = empName;
-        this._age = age;
-        this._empJob = empJob;
     }
-
     // get / set
     get empName() {
         return this._empName;
@@ -26,5 +20,5 @@ class Employee {
 }
 
 let employee1 = new Employee('lim', 21, 'developer');
-// employee1.empName = 'hanbi';
+employee1.empName = 'hanbi';
 employee1.printEmp();
