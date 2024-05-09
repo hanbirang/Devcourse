4277c4d03259379704acd8abb5477bb41ac8cc28
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-09 10:22:49 +0900

    [ w11d3 ] getter와 setter 컴파일

--- TypeScript/oop.js
+++ TypeScript/oop.js
@@ -4,15 +4,25 @@ var Employee = /** @class */ (function () {
     function Employee(empName, age, empJob) {
         var _this = this;
         this.printEmp = function () {
-            console.log("".concat(_this.empName, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this.empJob, "\uC785\uB2C8\uB2E4."));
+            console.log("".concat(_this._empName, "\uC758 \uB098\uC774\uB294 ").concat(_this._age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this._empJob, "\uC785\uB2C8\uB2E4."));
         };
-        this.empName = empName;
-        this.age = age;
-        this.empJob = empJob;
+        this._empName = empName;
+        this._age = age;
+        this._empJob = empJob;
     }
+    Object.defineProperty(Employee.prototype, "empName", {
+        // get / set
+        get: function () {
+            return this._empName;
+        },
+        set: function (val) {
+            this._empName = val;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
-// public private protected : 데이터 접근을 제한하기 위해 사용 
 var employee1 = new Employee('lim', 21, 'developer');
 // employee1.empName = 'hanbi';
 employee1.printEmp();
