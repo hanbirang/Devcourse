8049cdf026900f8672d885b3ff00045ee4511782
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-10 19:26:14 +0900

    [ w11d5 ] App.tsx에 함수형 컴포넌트 import하기

--- ReactExam/todolist/src/App.tsx
+++ ReactExam/todolist/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ClassCom from './ClassCom';
+import FuncCom from './FuncCom';
 
 function App() {
   let name = "리액트";
   return (
     <div className="container">
       <ClassCom></ClassCom>
+      <FuncCom></FuncCom>
     </div>
   );
 }
