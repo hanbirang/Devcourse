14b35710ad4eac071583d16a391c0f3ab69733b1
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-10 19:15:27 +0900

    [ w11d5 ] App.tsx에서 클래스형 컴포넌트 import 하기

--- ReactExam/todolist/src/App.tsx
+++ ReactExam/todolist/src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ClassCom from './ClassCom';
 
 function App() {
   let name = "리액트";
   return (
     <div className="container">
-      <h1 className = "test">Hello, 
-      {
-        name === '리액트' ? (<h1>YES</h1>) : null
-      }!!</h1>
-      <p>반갑습니다.</p>
-      <br />
-      {/* 주석문을 작성합니다. */}
+      <ClassCom></ClassCom>
     </div>
   );
 }
