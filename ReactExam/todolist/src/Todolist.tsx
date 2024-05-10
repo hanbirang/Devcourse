a175c92a0cb8dbe856f5f7d0c4f29c8a317d4052
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-10 20:17:29 +0900

    [ w11d5 ] state(useState)를 이용하여 데이터를 바인딩하기

--- ReactExam/todolist/src/Todolist.tsx
+++ ReactExam/todolist/src/Todolist.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoList : React.FC = () => {
     const title : string = "오늘 할 일";
+    const [todos] = useState(['공부하기', '잠자기', '미팅하기']);
     return(
-        <div className='container'>
+        <div>
             <h1>{title}</h1>
+            <p></p>
+            <div className='container'>
+                <ul>
+                    <li>{todos[0]}</li>
+                    <li>{todos[1]}</li>
+                    <li>{todos[2]}</li>
+                </ul>
+            </div>
         </div>
     );
 }
