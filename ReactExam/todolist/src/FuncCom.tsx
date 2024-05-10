3bc1f1cebe55955da37f5359426c59bfafb96639
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-10 19:25:43 +0900

    [ w11d5 ] 함수형 컴포넌트 만들기

---
+++ ReactExam/todolist/src/FuncCom.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FuncCom = () =>
+// function FuncCom()
+{
+    return(
+        <div>
+            함수형 컴포넌트
+        </div>
+    );
+}
+
+export default FuncCom;
