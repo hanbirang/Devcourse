e409900925bac8310b5c339be967f61959cefdb8
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-12 21:34:56 +0900

    [ w11d5 ] todolist만 보이게 수정

--- ReactExam/todolist/src/App.tsx
+++ ReactExam/todolist/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './Todolist';
-import MapTest from './MapTest';
 
 function App() {
   let name = "리액트";
   return (
     <div className="container">
       <TodoList></TodoList>
-      <MapTest></MapTest>
     </div>
   );
 }
