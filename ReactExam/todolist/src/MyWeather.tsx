8ed6d1fb9d32acee9a339527abeac43999c8c72c
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-13 21:07:40 +0900

    [ w12d1 ] class형 component에서의 props

--- ReactExam/todolist/src/MyWeather.tsx
+++ ReactExam/todolist/src/MyWeather.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Component } from "react";
 
 interface MyProps {
     weather : string;
     children : React.ReactNode;
 }
 
-const MyWeather : React.FC<MyProps> = ({children, weather}) => {
-    return (
-        <div>
-            {children}<p />
-            오늘의 날씨는 {weather} 입니다. 
-        </div>
-    )
+// const MyWeather : React.FC<MyProps> = ({children, weather}) => {
+//     return (
+//         <div>
+//             {children}<p />
+//             오늘의 날씨는 {weather} 입니다. 
+//         </div>
+//     )
+// }
+
+class MyWeather extends Component<MyProps> {
+    render() {
+        const {children, weather} = this.props;
+        return (
+            <div>
+                {children}<p />
+                오늘의 날씨는 {weather} 입니다.
+            </div>
+        )
+    }
 }
 
 export default MyWeather;
