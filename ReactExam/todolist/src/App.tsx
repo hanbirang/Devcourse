85ef0be19387e9432e503996c7c3fca6df32d01e
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-13 20:58:56 +0900

    [ w12d1 ] MyWeather import 후 동적으로 날씨 구현

--- ReactExam/todolist/src/App.tsx
+++ ReactExam/todolist/src/App.tsx
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import TodoList from './Todolist';
 import Clock from './Timer';
+import MyWeather from './MyWeather';
 
 function App() {
-  let name = "리액트";
   return (
     <div className="container">
       <TodoList />
       <Clock />
+      <MyWeather weather='맑음'/>
     </div>
   );
 }
