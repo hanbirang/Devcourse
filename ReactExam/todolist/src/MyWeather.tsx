9d0a3cbd5aebddce7c3124b7a248494a90710f4d
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-13 21:00:47 +0900

    [ w12d1 ] props의 children을 이용하여 컴포넌트의 자식 요소 저장

--- ReactExam/todolist/src/MyWeather.tsx
+++ ReactExam/todolist/src/MyWeather.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 interface MyProps {
     weather : string;
+    children : React.ReactNode;
 }
 
 const MyWeather : React.FC<MyProps> = (props) => {
     return (
         <div>
+            {props.children}<p />
             오늘의 날씨는 {props.weather} 입니다. 
         </div>
     )
