9170f0ac2c5121fd800823220f7e75c8949a7749
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-13 20:58:10 +0900

    [ w12d1 ] props를 사용하여 동적으로 날씨 출력

---
+++ ReactExam/todolist/src/MyWeather.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface MyProps {
+    weather : string;
+}
+
+const MyWeather : React.FC<MyProps> = (props) => {
+    return (
+        <div>
+            오늘의 날씨는 {props.weather} 입니다. 
+        </div>
+    )
+}
+
+export default MyWeather;
