d4c763a7be138223da06a337579120569dad8489
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-13 19:27:10 +0900

    [ w12d1 ] Timer import

--- ReactExam/todolist/src/App.tsx
+++ ReactExam/todolist/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './Todolist';
+import Timer from './Timer';
 
 function App() {
   let name = "리액트";
   return (
     <div className="container">
       <TodoList></TodoList>
+      <Timer></Timer>
     </div>
   );
 }
