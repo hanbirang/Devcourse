5d510c26cc90a0e73eeb961e663ee7b51fa0c5ac
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-14 21:44:27 +0900

    [ w12d2 ] Board 데이터 생성

--- react-task-app/src/store/slices/boardsSlice.ts
+++ react-task-app/src/store/slices/boardsSlice.ts
@@ -1,8 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState = {
+type TBoardsState = {
+    modalActive : boolean;
+    boardArray : IBoard[];
+}
+const initialState : TBoardsState = {
     modalActive : false,
-    boardArray : []
+    boardArray : [
+        {
+            boardId : 'board-0',
+            boardName : "첫 번째 게시물",
+            lists : [
+                {
+                    listId : "list-0",
+                    listName : "List 1",
+                    tasks : [
+                        {
+                            taskId : "task-0",
+                            taskName : "Task 1",
+                            taskDescription : "Description",
+                            taskOwner : "Hanbirang"
+                        },
+                        {
+                            taskId : "task-1",
+                            taskName : "Task 2",
+                            taskDescription : "Description",
+                            taskOwner : "Hanbirang"
+                        }
+                    ]
+                },
+                {
+                    listId : "list-1",
+                    listName : "List 2",
+                    tasks : [
+                        {
+                            taskId : "task-3",
+                            taskName : "Task 3",
+                            taskDescription : "Description",
+                            taskOwner : "Hanbirang"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 }
 
 const boardsSlice = createSlice({
