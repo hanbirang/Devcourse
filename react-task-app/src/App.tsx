7bdcf73424cda32c8571f30188574fa4523f360d
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-16 21:14:26 +0900

    [ w12d4 ] feat: ListContainer 적용하기

--- react-task-app/src/App.tsx
+++ react-task-app/src/App.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import { appContainer, board, buttons } from "./App.css";
 import BoardList from "./components/BoardList/BoardList";
+import ListsContainer from "./components/ListsContainer/ListsContainer";
+import { useTypedSelector } from "./hooks/redux";
 
 function App() {
     const [activeBoardId, setActiveBoardId] = useState('board-0');
+
+    const boards = useTypedSelector(state => state.boards.boardArray)
+
+    const getActiveBoard = boards.filter(board => board.boardId === activeBoardId)[0];
+
+    const lists = getActiveBoard.lists;
+
     return (
         <div className = {appContainer}>
             <BoardList 
@@ -11,7 +20,7 @@ function App() {
                 setActiveBoardId = {setActiveBoardId}
             />
             <div className={board}>
-                
+                <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
             </div>
             <div className={buttons}>
                 <button>
