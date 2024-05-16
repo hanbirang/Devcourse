0cc38ad2a7a21e1feb82979e1b503a2cfe44600c
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-16 20:45:42 +0900

    [ w12d4 ] feat: 버튼을 누르고 입력하면 새 board 생성 기능 추가

--- react-task-app/src/store/slices/boardsSlice.ts
+++ react-task-app/src/store/slices/boardsSlice.ts
@@ -1,9 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IBoard } from "../../types";
 
 type TBoardsState = {
     modalActive : boolean;
     boardArray : IBoard[];
+}
+
+type TAddBoardAction = {
+    board: IBoard;
 }
 const initialState : TBoardsState = {
     modalActive : false,
@@ -51,8 +55,11 @@ const boardsSlice = createSlice({
     name : 'boards',
     initialState,
     reducers : {
-
+        addBoard: (state, {payload}: PayloadAction<TAddBoardAction>) => {
+            state.boardArray.push(payload.board);
+        }
     }
 });
 
+export const {addBoard} = boardsSlice.actions;
 export const boardsReducer = boardsSlice.reducer;
