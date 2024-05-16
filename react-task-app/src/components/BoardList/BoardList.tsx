4ff45f1c6941772d98bdee27a87ba6b5a3eb4fe0
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-16 15:53:57 +0900

    [ w12d4 ] feat: Board List Style 적용하기

--- react-task-app/src/components/BoardList/BoardList.tsx
+++ react-task-app/src/components/BoardList/BoardList.tsx
@@ -2,6 +2,8 @@ import React, { FC, useState } from 'react'
 import { useTypedSelector } from '../../hooks/redux';
 import SideForm from './SideForm/SideForm';
 import { FiPlusCircle } from 'react-icons/fi';
+import { addButton, addSection, boardItem, boardItemActive, container, title } from './BoardList.css';
+import clsx from 'clsx';
 
 type TBoardListProps = {
   activeBoardId : string;
@@ -15,23 +17,37 @@ const BoardList: FC<TBoardListProps> = ({
   const {boardArray} = useTypedSelector(state =>state.boards);
   const [isFormOpen, setIsFormOpen] = useState(false);
   return (
-    <div>
-      <div>
+    <div className={container}>
+      <div className={title}>
         게시판 : 
       </div>
       {boardArray.map((board, index) => (
-        <div key={board.boardId}>
+        <div key={board.boardId}
+        onClick={()=>setActiveBoardId(boardArray[index].boardId)}
+          className={
+            clsx(
+              {
+                [boardItemActive]:
+                boardArray.findIndex(b =>board.boardId === activeBoardId) === index
+              },
+              {
+                [boardItem] : 
+                boardArray.findIndex(b =>board.boardId === activeBoardId)!== index
+              }
+            )
+          }
+        >
             <div>
               {board.boardName}
             </div>
         </div>
       ))}
-      <div>
+      <div className={addSection}>
         {
           isFormOpen ?
             <SideForm setIsFormOpen={setIsFormOpen}/>
           :
-            <FiPlusCircle onClick={() => setIsFormOpen(!isFormOpen)}/>
+            <FiPlusCircle className={addButton} onClick={() => setIsFormOpen(!isFormOpen)}/>
         }
       </div>
     </div>
