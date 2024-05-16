91a1650a110b7c7dd74e0f5066486df49f98474c
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-16 21:51:58 +0900

    [ w12d4 ] feat(List): List Component 생성 및 기능 구현
    버튼을 누르면 리스트를 삭제하고, 로그를 기록하는 기능 구현

--- react-task-app/src/components/List/List.tsx
+++ react-task-app/src/components/List/List.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { GrSubtract } from 'react-icons/gr'
+import Task from '../Task/Task'
+import ActionButton from '../ActionButton/ActionButton'
+import { IList } from '../../types'
+import { useTypedDispatch } from '../../hooks/redux'
+import { deleteList } from '../../store/slices/boardsSlice'
+import { addLog } from '../../store/slices/loggerSlice'
+import { v4 } from 'uuid'
 
-const List = ({
+type TListProps = {
+  boardId: string;
+  list: IList;
+}
+
+const List: FC<TListProps> = ({
   list,
   boardId
 }) => {
+  const dispatch = useTypedDispatch();
+
+  const handleListDelete = (listId: string) => {
+    dispatch(deleteList({boardId, listId}));
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `리스트 삭제하기: ${list.listName}`,
+        logAuthor: "User",
+        logTimestamp: String(new Date())
+      })
+    )
+  }
+
   return (
-    <div>List</div>
+    <div>
+      <div>
+        <div>{list.listName}</div>
+        <GrSubtract 
+          onClick={() => handleListDelete(list.listId)}
+        />
+      </div>
+      {list.tasks.map((task, index) => (
+        <div
+          key={task.taskId}
+        >
+          <Task 
+            taskName={task.taskName}
+            taskDescription={task.taskDescription}
+            boardId={boardId}
+            id={task.taskId}
+            index={index}   // drag and drop 기능을 위해 전달
+          />
+        </div>
+      ))}
+      <ActionButton />
+    </div>
   )
 }
 
