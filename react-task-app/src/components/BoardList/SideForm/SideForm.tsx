5bb1f0fe78277fbeb18f72cd0051a8b68059fcd8
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-16 20:11:18 +0900

    [ w12d4 ] feat: SideForm 생성하기
    플러스 버튼을 누르면 텍스트 입력 창이 나온다. 자동으로 포커싱 하기 위해 autoFocus 사용. 그럼 inputRef가 없어도 된다. 리팩토링할 때 수정할 것

--- react-task-app/src/components/BoardList/SideForm/SideForm.tsx
+++ react-task-app/src/components/BoardList/SideForm/SideForm.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { ChangeEvent, FC, useState } from 'react';
+import { FiCheck } from 'react-icons/fi';
 
-const SideForm = () => {
+type TSideFormProps = {
+  inputRef : React.RefObject<HTMLInputElement>;
+  setIsFormOpen : React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SideForm :FC<TSideFormProps> = ({
+  setIsFormOpen,
+  inputRef
+}) => {
+  const [inputText, setInputText] = useState('');
+  
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  }
+
+  const handleOnBlur = () => {
+    setIsFormOpen(false);
+  }
+
   return (
-    <div>SideForm</div>
+    <div>
+      <input 
+        // ref = {inputRef}
+        autoFocus
+        type='text'
+        placeholder='새로운 게시판 등록하기'
+        value={inputText}
+        onChange={handleChange}
+        onBlur={handleOnBlur}
+      />
+      <FiCheck />
+    </div>
   )
 }
 
