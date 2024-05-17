ba5a909839e3055f5a846618de6a304a36ee6510
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-17 11:51:31 +0900

    [ w12d4 ] feat(DropDownForm): DropDownForm 컴포넌트 생성하기
    props에서 list의 유무에 따라 리스트의 제목 입력인지 일의 제목 입력인지 분기 처리함. 또한 리스트 추가인지 일 추가인지 분기 처리함.

--- react-task-app/src/components/ActionButton/DropDownForm/DropDownForm.tsx
+++ react-task-app/src/components/ActionButton/DropDownForm/DropDownForm.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { ChangeEvent, FC, useState } from 'react'
+import { FiX } from 'react-icons/fi';
 
-const DropDownForm = () => {
+type TDropDownFormProps = {
+  boardId: string;
+  listId: string;
+  setIsFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  list?: boolean;
+}
+
+const DropDownForm: FC<TDropDownFormProps> = ({
+  boardId,
+  list,
+  listId,
+  setIsFormOpen
+}) => {
+  const [text, setText] = useState('');
+  const formPlaceholder = list ? 
+      "리스트의 제목을 입력하세요." : 
+      "일의 제목을 입력하세요.";
+
+  const buttonTitle = list ?
+      "리스트 추가하기" :
+      "일 추가하기";
+
+  const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  }
+
   return (
-    <div>DropDownForm</div>
+    <div>
+      <textarea 
+        value={text}
+        onChange={handleTextChange}
+        autoFocus
+        placeholder={formPlaceholder}
+        onBlur={() => setIsFormOpen(false)}
+      />
+      <div>
+        <button>
+          {buttonTitle}
+        </button>
+        <FiX />
+      </div>
+    </div>
   )
 }
 
