95ffce649de4a99bfb9937a68a4066066e210221
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-17 12:16:37 +0900

    [ w12d4 ] feat(DropDownForm): addTask, addList 기능 생성
    분기 처리를 통해 버튼을 누르면 task, list 추가

--- react-task-app/src/components/ActionButton/DropDownForm/DropDownForm.tsx
+++ react-task-app/src/components/ActionButton/DropDownForm/DropDownForm.tsx
@@ -1,5 +1,9 @@
 import React, { ChangeEvent, FC, useState } from 'react'
 import { FiX } from 'react-icons/fi';
+import { useTypedDispatch } from '../../../hooks/redux';
+import { v4 } from 'uuid';
+import { addList, addTask } from '../../../store/slices/boardsSlice';
+import { addLog } from '../../../store/slices/loggerSlice';
 
 type TDropDownFormProps = {
   boardId: string;
@@ -14,6 +18,7 @@ const DropDownForm: FC<TDropDownFormProps> = ({
   listId,
   setIsFormOpen
 }) => {
+  const dispatch = useTypedDispatch();
   const [text, setText] = useState('');
   const formPlaceholder = list ? 
       "리스트의 제목을 입력하세요." : 
@@ -27,6 +32,54 @@ const DropDownForm: FC<TDropDownFormProps> = ({
     setText(e.target.value);
   }
 
+  const handleButtonClick = () => {
+    if (text) {
+      if (list) {
+        dispatch(
+          addList({
+            boardId,
+            list: {
+              listId: v4(),
+              listName: text,
+              tasks: []
+            }
+          })
+        );
+
+        dispatch(
+          addLog({
+            logId: v4(),
+            logMessage: `리스트 생성하기: ${text}`,
+            logAuthor: "User",
+            logTimestamp: String(Date.now()),
+          })
+        );
+      } else {
+        dispatch(
+          addTask({
+            boardId,
+            listId,
+            task: {
+              taskId: v4(),
+              taskName: text,
+              taskDescription: "",
+              taskOwner: "User"
+            }
+          })
+        );
+
+        dispatch(
+          addLog({
+            logId: v4(),
+            logMessage: `일 생성하기: ${text}`,
+            logAuthor: "User",
+            logTimestamp: String(Date.now()),
+          })
+        );
+      }
+    }
+  }
+
   return (
     <div>
       <textarea 
@@ -37,7 +90,7 @@ const DropDownForm: FC<TDropDownFormProps> = ({
         onBlur={() => setIsFormOpen(false)}
       />
       <div>
-        <button>
+        <button onMouseDown={handleButtonClick}>
           {buttonTitle}
         </button>
         <FiX />
