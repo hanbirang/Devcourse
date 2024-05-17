3b9576830454ec228e95ebe9eac3f351e8031a2d
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-17 10:11:21 +0900

    [ w12d4 ] feat: Action Button Component 생성하기
    props에서 list 유무에 따라 리스트 등록인지 일 등록인지 달라짐

--- react-task-app/src/components/ActionButton/ActionButton.tsx
+++ react-task-app/src/components/ActionButton/ActionButton.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { FC, useState } from 'react'
+import DropDownForm from './DropDownForm/DropDownForm';
+import { IoIosAdd } from 'react-icons/io';
 
-const ActionButton = () => {
-  return (
-    <div>ActionButton</div>
+type TActionButtonProps = {
+  boardId: string,
+  listId: string,
+  list?: boolean,
+}
+
+const ActionButton : FC<TActionButtonProps> = (
+  boardId,
+  listId,
+  list
+) => {
+
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const buttonText = list? "새로운 리스트 등록" : "새로운 일 등록";
+  
+  return isFormOpen ? (
+    <DropDownForm 
+      setIsFormOpen = {setIsFormOpen}
+      list = {list ? true : false}
+      boardId = {boardId}
+      listId = {listId}
+    />
   )
+    :
+    (
+      <div>
+        <IoIosAdd />
+        <p>{buttonText}</p>
+      </div>
+    )
 }
 
 export default ActionButton
