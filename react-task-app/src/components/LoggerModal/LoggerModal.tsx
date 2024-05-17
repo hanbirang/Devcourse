0547287f1f492730a8809e4d9f40108285ffc0a3
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-17 22:11:08 +0900

    [ w12d5 ] style(LoggerModal): Logger Modal style 적용하기

--- react-task-app/src/components/LoggerModal/LoggerModal.tsx
+++ react-task-app/src/components/LoggerModal/LoggerModal.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { useTypedSelector } from '../../hooks/redux'
 import { FiX } from 'react-icons/fi'
 import LogItem from './LogItem/LogItem'
+import { body, closeButton, header, modalWindow, title, wrapper } from './LoggerModal.css'
 
 type TLoggerModalProps = {
   setIsLoggerOpen: React.Dispatch<React.SetStateAction<boolean>>
@@ -13,13 +14,13 @@ const LoggerModal: FC<TLoggerModalProps> = ({
   const logs = useTypedSelector(state => state.logger.logArray);
 
   return (
-    <div>
-      <div>
-        <div>
-          <div>활동 기록</div>
-          <FiX onClick={() => setIsLoggerOpen(false)} />
+    <div className={wrapper}>
+      <div className={modalWindow}>
+        <div className={header}>
+          <div className={title}>활동 기록</div>
+          <FiX className={closeButton} onClick={() => setIsLoggerOpen(false)} />
         </div>
-        <div>
+        <div className={body}>
           {logs.map((log, index) => (
             <LogItem key={log.logId} logItem={log} />
           ))}
