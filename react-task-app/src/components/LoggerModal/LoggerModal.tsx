933cf21806884c5dbdfdbb1c047e3e72ef30b5ba
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-17 21:30:06 +0900

    [ w12d5 ] feat(App): Logger Modal 컴포넌트 생성 (UI 생성)

--- react-task-app/src/components/LoggerModal/LoggerModal.tsx
+++ react-task-app/src/components/LoggerModal/LoggerModal.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { useTypedSelector } from '../../hooks/redux'
+import { FiX } from 'react-icons/fi'
+import LogItem from './LogItem/LogItem'
 
-const LoggerModal = () => {
+type TLoggerModalProps = {
+  setIsLoggerOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const LoggerModal: FC<TLoggerModalProps> = ({
+  setIsLoggerOpen
+}) => {
+  const logs = useTypedSelector(state => state.logger.logArray);
+
   return (
-    <div>LoggerModal</div>
+    <div>
+      <div>
+        <div>
+          <div>활동 기록</div>
+          <FiX onClick={() => setIsLoggerOpen(false)} />
+        </div>
+        <div>
+          {logs.map((log, index) => (
+            <LogItem key={log.logId} logItem={log} />
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
