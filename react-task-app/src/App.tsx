1f3e6f5c4fba9cb8c17c9fe5abcf773e7e2969de
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-17 21:23:03 +0900

    [ w12d5 ] feat(App): Logger Modal 컴포넌트 적용

--- react-task-app/src/App.tsx
+++ react-task-app/src/App.tsx
@@ -4,8 +4,10 @@ import BoardList from "./components/BoardList/BoardList";
 import ListsContainer from "./components/ListsContainer/ListsContainer";
 import { useTypedSelector } from "./hooks/redux";
 import EditModal from "./components/EditModal/EditModal";
+import LoggerModal from "./components/LoggerModal/LoggerModal";
 
 function App() {
+    const [isLoggerOpen, setIsLoggerOpen] = useState(false);
     const [activeBoardId, setActiveBoardId] = useState('board-0');
     const modalActive = useTypedSelector(state => state.boards.modalActive);
     const boards = useTypedSelector(state => state.boards.boardArray)
@@ -16,8 +18,8 @@ function App() {
 
     return (
         <div className = {appContainer}>
-
-            {modalActive ? <EditModal /> : null }
+            {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
+            {modalActive ? <EditModal /> : null}
             <BoardList 
                 activeBoardId = {activeBoardId} 
                 setActiveBoardId = {setActiveBoardId}
@@ -29,8 +31,8 @@ function App() {
                 <button>
                     이 게시판 삭제하기
                 </button>
-                <button>
-
+                <button onClick={() => setIsLoggerOpen(!isLoggerOpen)}>
+                    {isLoggerOpen ? "활동 목록 숨기기" : "활동 목록 보이기"}
                 </button>
             </div>
         </div>
