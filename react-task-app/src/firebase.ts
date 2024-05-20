325696c79d1a968a2bafe38383d120f77b05e3f6
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-20 20:58:22 +0900

    [ w13d1 ] feat(firebase): Firebase 연동하기

---
+++ react-task-app/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCB5Tcvl9sNrgZd1x5eHW_Ew2YinHblmMg",
+    authDomain: "react-test-app-24873.firebaseapp.com",
+    projectId: "react-test-app-24873",
+    storageBucket: "react-test-app-24873.appspot.com",
+    messagingSenderId: "69615786488",
+    appId: "1:69615786488:web:e2441bcb69d00901a85e0d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
