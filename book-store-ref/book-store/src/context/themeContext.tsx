7aefce140ebef02c4a0ab6a78d2313ba1948c033
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-30 22:26:18 +0900

    [ w14d3 ] feat(themeContext)

--- book-store-ref/book-store/src/context/themeContext.tsx
+++ book-store-ref/book-store/src/context/themeContext.tsx
@@ -1,22 +1,33 @@
-import { ReactNode, createContext } from "react";
-import { ThemeName } from "../style/theme";
+import { ReactNode, createContext, useState } from "react";
+import { ThemeName, getTheme } from "../style/theme";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../style/global";
 
 interface State {
     themeName: ThemeName;
-    setThemeName: (themeName: ThemeName) => void;
+    toggleTheme: () => void;
 }
 
 export const state = {
     themeName: "light" as ThemeName,
-    setThemeName: (themeName: ThemeName) => {}
+    toggleTheme: () => {}
 };
 
 export const ThemeContext = createContext<State>(state);
 
 export const BookStoreThemeProvider = ({children}: {children: ReactNode}) => {
+    const [themeName, setThemeName] = useState<ThemeName>("light");
+    
+    const toggleTheme =() => {
+        setThemeName(themeName === "light" ? "dark" : "light");
+    }
+    
     return (
-        <ThemeContext.Provider value={state}>
-            {children}
+        <ThemeContext.Provider value={{themeName, toggleTheme}}>
+            <ThemeProvider theme={getTheme(themeName)}>
+                <GlobalStyle themeName={themeName} />
+                    {children}
+            </ThemeProvider>
         </ThemeContext.Provider>
     );
 }
