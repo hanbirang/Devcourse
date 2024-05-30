c5e03ba85650dd9f3ae8ab83f324e87cf14188e6
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-30 12:40:04 +0900

    [ w14d3 ] feat(Home): Footer 적용하기

--- book-store-ref/book-store/src/pages/Home.tsx
+++ book-store-ref/book-store/src/pages/Home.tsx
@@ -1,13 +1,12 @@
+import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
-import { formatNumber } from "../utils/format";
 
-const COUNT = 10000;
 function Home() {
     return (
         <>
             <Header />
             <div>home body</div>
-            <div>count: {formatNumber(COUNT)}</div>
+            <Footer />
         </>
     );
 }
