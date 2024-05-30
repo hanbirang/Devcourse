c1bf046ea927e0f412ce55bcd0a7634f35c94437
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-30 15:41:06 +0900

    [ w14d3 ] feat(Home): header와 footer 삭제

--- book-store-ref/book-store/src/pages/Home.tsx
+++ book-store-ref/book-store/src/pages/Home.tsx
@@ -1,12 +1,7 @@
-import Footer from "../components/common/Footer";
-import Header from "../components/common/Header";
-
 function Home() {
     return (
         <>
-            <Header />
             <div>home body</div>
-            <Footer />
         </>
     );
 }
