e645b30b9d20a5b8384442055fff898dc48d0dc0
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-31 17:09:40 +0900

    [ w14d4 ] refactor(App): import 정리

--- book-store-ref/book-store/src/App.tsx
+++ book-store-ref/book-store/src/App.tsx
@@ -1,6 +1,5 @@
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
-import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { BookStoreThemeProvider } from "./context/themeContext";
 
 function App() {
