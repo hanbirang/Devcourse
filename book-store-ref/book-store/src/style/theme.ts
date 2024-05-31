3b1413371bd34b8f532fff19276a83551f33d461
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-31 13:36:02 +0900

    [ w14d4 ] style(theme): button theme style 지정

--- book-store-ref/book-store/src/style/theme.ts
+++ book-store-ref/book-store/src/style/theme.ts
@@ -2,6 +2,8 @@ export type ThemeName = "light" | "dark";
 export type ColorKey = "primary" | "background" | "secondary" | "third";
 
 export type HeadingSize = "large" | "medium" | "small";
+export type ButtonSize = "large" | "medium" | "small";
+export type ButtonScheme = "primary" | "normal";
 
 interface Theme {
     name: ThemeName;
@@ -11,13 +13,28 @@ interface Theme {
             fontSize: string;
         };
     };
+    button: {
+        [key in ButtonSize]: {
+            fontSize: string;
+            padding: string;
+        }
+    };
+    buttonScheme: {
+        [key in ButtonScheme]: {
+            color: string;
+            backgroundColor: string;
+        }
+    };
+    borderRadius: {
+        default: string;
+    }
 }
 
 export const light: Theme = {
     name: 'light',
     color: {
         primary: 'brown',
-        background: 'lightgray',
+        background: 'lightgrey',
         secondary: 'blue',
         third: 'green',
     },
@@ -31,6 +48,33 @@ export const light: Theme = {
         small: {
             fontSize: "1rem"
         }
+    },
+    button: {
+        large: {
+            fontSize: "1.5rem",
+            padding: "1rem 2rem"
+        },
+        medium: {
+            fontSize: "1rem",
+            padding: "0.5rem 1rem"
+        },
+        small: {
+            fontSize: "0.75rem",
+            padding: "0.25rem 0.5rem"
+        }
+    },
+    buttonScheme: {
+        primary: {
+            color: 'white',
+            backgroundColor: 'midnightblue'
+        },
+        normal: {
+            color: 'black',
+            backgroundColor: 'lightgrey'
+        }
+    },
+    borderRadius: {
+        default: "4px"
     }
 };
 
