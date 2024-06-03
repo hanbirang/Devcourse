6842a82bca0655ca1b870db24456ad15d0f4a72b
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-03 22:14:47 +0900

    [ w14d5 ] feat(App): router에 로그인 경로 추가

--- book-store-ref/book-store/src/App.tsx
+++ book-store-ref/book-store/src/App.tsx
@@ -5,6 +5,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Error from "./components/common/Error";
 import Signup from "./pages/Signup";
 import ResetPassword from "./pages/ResetPassword";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -30,6 +31,12 @@ const router = createBrowserRouter([
     path: "/reset",
     element: (
       <Layout><ResetPassword /></Layout>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <Layout><Login /></Layout>
     ),
   }
 ]);
