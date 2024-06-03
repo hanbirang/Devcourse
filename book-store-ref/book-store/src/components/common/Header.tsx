fcca687f629abc77fadeec7ba1f685458b8b1205
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-03 19:04:09 +0900

    [ w14d4 ] fix(Header): 백엔드와의 연동을 위해 category model 수정

--- book-store-ref/book-store/src/components/common/Header.tsx
+++ book-store-ref/book-store/src/components/common/Header.tsx
@@ -18,11 +18,11 @@ function Header() {
                 <ul>
                     {
                         category.map((item) => (
-                            <li key={item.id}>
+                            <li key={item.category_id}>
                                 <Link to={
-                                    item.id === null ? `/books` : `/books?category_id=${item.id}`
+                                    item.category_id === null ? `/books` : `/books?category_id=${item.category_id}`
                                 }>
-                                    {item.name}
+                                    {item.category_name}
                                 </Link>
                             </li>
                         ))
