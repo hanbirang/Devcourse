e20e00d1020103e48a1eaf9193526048c9ee5ab5
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 21:12:15 +0900

    [ w14d5 ] feat(Pagination): Pagination button에 대한 handleClickPage 기능 추가
    버튼을 누르면 쿼리 스트링 추가.
    버튼 누르면 목록 변경

--- book-store-ref/book-store/src/components/books/Pagination.tsx
+++ book-store-ref/book-store/src/components/books/Pagination.tsx
@@ -2,13 +2,23 @@ import { styled } from 'styled-components';
 import { Pagination as IPagination } from '../../models/pagination.model';
 import { LIMIT } from '../../constants/pagination';
 import Button from '../common/Button';
+import { useSearchParams } from 'react-router-dom';
+import { QUERYSTRING } from '../../constants/querystring';
 
 interface Props {
    pagination: IPagination;
 }
 function Pagination({pagination}: Props) {
+   const [searchParams, setSearchParams] = useSearchParams();
    const { totalCount, currentPage } = pagination;
    const pages: number = Math.ceil(totalCount / LIMIT);
+
+   const handleClickPage = (page:number) => {
+      const newSearchParams = new URLSearchParams(searchParams);
+
+      newSearchParams.set(QUERYSTRING.PAGE, page.toString());
+      setSearchParams(newSearchParams);
+   }
 
    return (
       <PaginationStyle>
@@ -18,7 +28,11 @@ function Pagination({pagination}: Props) {
                   {
                      Array(pages).fill(0).map((_, index) => (
                         <li>
-                           <Button size='small' scheme='normal'>
+                           <Button 
+                              key={index} size='small' 
+                              scheme='normal'
+                              onClick={() => handleClickPage(index + 1)}
+                           >
                               {index + 1}
                            </Button>
                         </li>
