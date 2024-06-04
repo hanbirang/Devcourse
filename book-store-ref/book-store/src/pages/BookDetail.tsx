9d27e2a8196aa82927bcb5cbf37014969b2a54e1
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 22:28:22 +0900

    [ w15d1 ] feat(BookDetail): BookDetail 페이지 틀 생성

---
+++ book-store-ref/book-store/src/pages/BookDetail.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+function BookDetail() {
+   return (
+    <BookDetailStyle></BookDetailStyle>
+   );
+}
+
+const BookDetailStyle = styled.div`
+`;
+
+export default BookDetail;
