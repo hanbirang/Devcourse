05fb171ef8c5d75233c8508a1f885098df847f47
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 21:24:57 +0900

    [ w14d5 ] feat(BooksViewSwitcher): view Switcher 아이콘 (그리드, 리스트) 넣기

--- book-store-ref/book-store/src/components/books/BooksViewSwitcher.tsx
+++ book-store-ref/book-store/src/components/books/BooksViewSwitcher.tsx
@@ -1,8 +1,29 @@
 import { styled } from 'styled-components';
+import Button from '../common/Button';
+import { FaList, FaTh } from 'react-icons/fa';
+
+const viewOptions = [
+   {
+      value: 'list',
+      icon: <FaList />
+   },
+   {
+      value: 'grid',
+      icon: <FaTh />
+   }
+]
 
 function BooksViewSwitcher() {
    return (
-    <BooksViewSwitcherStyle></BooksViewSwitcherStyle>
+      <BooksViewSwitcherStyle>
+         {
+            viewOptions.map((option) => (
+               <Button size='medium' scheme='normal'>
+                  {option.icon}
+               </Button>
+            ))
+         }
+      </BooksViewSwitcherStyle>
    );
 }
 
