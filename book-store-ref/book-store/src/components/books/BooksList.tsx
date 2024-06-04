0e8d3cee692991d8b09af2017fe5ca70af909d34
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 10:17:56 +0900

    [ w14d5 ] feat(BooksList): BooksList 컴포넌트 생성

---
+++ book-store-ref/book-store/src/components/books/BooksList.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+function BooksList() {
+   return (
+    <BooksListStyle></BooksListStyle>
+   );
+}
+
+const BooksListStyle = styled.div`
+`;
+
+export default BooksList;
