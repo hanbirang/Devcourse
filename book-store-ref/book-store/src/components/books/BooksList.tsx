11b3465fb74a37199f68bd0c9a658a0100dfd6b3
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 20:19:19 +0900

    [ w14d5 ] feat(BooksList): Book 목록 보이기

--- book-store-ref/book-store/src/components/books/BooksList.tsx
+++ book-store-ref/book-store/src/components/books/BooksList.tsx
@@ -2,27 +2,18 @@ import { styled } from 'styled-components';
 import BookItem from './BookItem';
 import { Book } from '../../models/book.model';
 
-const dummyBook: Book = {
-   id: 1,
-   title: "Dummy Book",
-   img: 5,
-   category_id: 1,
-   summary: "Dummy Summary",
-   author: "Dummy Author",
-   price: 10000,
-   likes: 1,
-   form: "paperback",
-   isbn: "Dummy ISBN",
-   detail: "Dummy Detail",
-   pages: 100,
-   contents: "Dummy Contents",
-   pubDate: "2021-01-01"
-};
+interface Props {
+   books: Book[];
+}
 
-function BooksList() {
+function BooksList({ books}: Props) {
    return (
       <BooksListStyle>
-         <BookItem book={dummyBook} />
+         {
+            books?.map((item) => (
+               <BookItem key={item.id} book={item} />
+            ))
+         }
       </BooksListStyle>
    );
 }
