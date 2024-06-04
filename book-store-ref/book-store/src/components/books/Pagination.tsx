2f41a60f2eb51b4b520790086581c8dc603bb333
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 21:03:55 +0900

    [ w14d5 ] feat(Pagination): Pagination 버튼 생성

--- book-store-ref/book-store/src/components/books/Pagination.tsx
+++ book-store-ref/book-store/src/components/books/Pagination.tsx
@@ -1,8 +1,33 @@
 import { styled } from 'styled-components';
+import { Pagination as IPagination } from '../../models/pagination.model';
+import { LIMIT } from '../../constants/pagination';
+import Button from '../common/Button';
 
-function Pagination() {
+interface Props {
+   pagination: IPagination;
+}
+function Pagination({pagination}: Props) {
+   const { totalCount, currentPage } = pagination;
+   const pages: number = Math.ceil(totalCount / LIMIT);
+
    return (
-    <PaginationStyle></PaginationStyle>
+      <PaginationStyle>
+         {
+            pages > 0 && (
+               <ol>
+                  {
+                     Array(pages).fill(0).map((_, index) => (
+                        <li>
+                           <Button size='small' scheme='normal'>
+                              {index + 1}
+                           </Button>
+                        </li>
+                     ))
+                  }
+               </ol>
+            )
+         }
+      </PaginationStyle>
    );
 }
 
