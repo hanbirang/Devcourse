15a1cfdcf5b3f92728e53eb46a1de6fff603230a
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 12:00:18 +0900

    [ w14d5 ] style(BooksEmpty): empty style 지정

--- book-store-ref/book-store/src/components/books/BooksEmpty.tsx
+++ book-store-ref/book-store/src/components/books/BooksEmpty.tsx
@@ -20,6 +20,19 @@ function BooksEmpty() {
 }
 
 const BooksEmptyStyle = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   gap: 12px;
+   padding: 120px 0;
+
+   .icon {
+      svg {
+         font-size: 4rem;
+         fill: #ccc;
+      }
+   }
 `;
 
 export default BooksEmpty;
