5ebd4b77602ab989d8b74c0040e61db673a69b0f
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 10:12:36 +0900

    [ w14d5 ] feat(Books): 도서 목록 페이지 틀 생성

---
+++ book-store-ref/book-store/src/pages/Books.tsx
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components';
+import Title from '../components/common/Title';
+
+function Books() {
+   return (
+    <>
+    <Title size='large'>도서 검색 결과</Title>
+        <BooksStyle>
+            {/* 필터 */}
+            {/* 목록 */}
+            {/* empty */}
+            {/* 페이지네이션 */}
+        </BooksStyle>
+    </>
+   );
+}
+
+const BooksStyle = styled.div`
+`;
+
+export default Books;
