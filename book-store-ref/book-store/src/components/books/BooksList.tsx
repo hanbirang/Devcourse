95aead6aa0c440998282e7c8517492b172468c73
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 21:44:58 +0900

    [ w14d5 ] feat(BooksList): querystring에 따라 view 값 설정

--- book-store-ref/book-store/src/components/books/BooksList.tsx
+++ book-store-ref/book-store/src/components/books/BooksList.tsx
@@ -1,14 +1,28 @@
 import { styled } from 'styled-components';
 import BookItem from './BookItem';
 import { Book } from '../../models/book.model';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { QUERYSTRING } from '../../constants/querystring';
+import { ViewMode } from './BooksViewSwitcher';
 
 interface Props {
    books: Book[];
 }
 
 function BooksList({ books}: Props) {
+   const [ view, setView ] = useState<ViewMode>('grid');
+   const location = useLocation();
+
+   useEffect(() => {
+      const params = new URLSearchParams(location.search);
+      if (params.get(QUERYSTRING.VIEW)) {
+         setView(params.get(QUERYSTRING.VIEW) as ViewMode);
+      }
+   }, [location.search]);
+
    return (
-      <BooksListStyle>
+      <BooksListStyle view={view}>
          {
             books?.map((item) => (
                <BookItem key={item.id} book={item} />
@@ -18,9 +32,15 @@ function BooksList({ books}: Props) {
    );
 }
 
-const BooksListStyle = styled.div`
+interface BooksListStyleProps {
+   view: ViewMode;
+}
+
+const BooksListStyle = styled.div<BooksListStyleProps>`
    display: grid;
-   grid-template-columns: repeat(4, 1fr);
+   grid-template-columns: ${({view}) => (
+      view === 'grid' ? 'repeat(4, 1fr);' : 'repeat(1, 1fr);'
+   )}
    gap: 24px;
 `;
 
