b01a129572a2ca17207ecea49f0eed9f5029a844
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 10:16:58 +0900

    [ w14d5 ] feat(BooksFilter): BooksFilter 컴포넌트 생성

---
+++ book-store-ref/book-store/src/components/books/BooksFilter.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+function BooksFilter() {
+   return (
+    <BooksFilterStyle></BooksFilterStyle>
+   );
+}
+
+const BooksFilterStyle = styled.div`
+`;
+
+export default BooksFilter;
