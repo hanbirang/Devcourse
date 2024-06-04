133064faa3ac74b84d89a40f284b1b89ca1b06b4
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 21:31:06 +0900

    [ w14d5 ] feat(BooksViewSwitcher): Switcher 버튼 클릭 시 쿼리 추가 & 버튼 색상 변경 기능 추가

--- book-store-ref/book-store/src/components/books/BooksViewSwitcher.tsx
+++ book-store-ref/book-store/src/components/books/BooksViewSwitcher.tsx
@@ -1,6 +1,8 @@
 import { styled } from 'styled-components';
 import Button from '../common/Button';
 import { FaList, FaTh } from 'react-icons/fa';
+import { useSearchParams } from 'react-router-dom';
+import { QUERYSTRING } from '../../constants/querystring';
 
 const viewOptions = [
    {
@@ -14,11 +16,28 @@ const viewOptions = [
 ]
 
 function BooksViewSwitcher() {
+   const [searchParams, setSearchParams] = useSearchParams();
+
+   const handleSwitch = (value: string) => {
+      const newSearchParams = new URLSearchParams(searchParams);
+
+      newSearchParams.set(QUERYSTRING.VIEW, value);
+      setSearchParams(newSearchParams);
+   }
+
    return (
       <BooksViewSwitcherStyle>
          {
             viewOptions.map((option) => (
-               <Button size='medium' scheme='normal'>
+               <Button 
+                  key={option.value}
+                  size='medium' 
+                  scheme={
+                     searchParams.get((QUERYSTRING.VIEW)) === option.value ?
+                     'primary' : 'normal'
+                  }
+                  onClick={() => handleSwitch(option.value)}
+               >
                   {option.icon}
                </Button>
             ))
