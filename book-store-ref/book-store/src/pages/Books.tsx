5005aae81bf746a686828baabb2b34a3ac2afc89
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 20:46:30 +0900

    [ w14d5 ] style(Books): Books 페이지 style 지정

--- book-store-ref/book-store/src/pages/Books.tsx
+++ book-store-ref/book-store/src/pages/Books.tsx
@@ -14,8 +14,10 @@ function Books() {
     <>
     <Title size='large'>도서 검색 결과</Title>
         <BooksStyle>
-            <BooksFilter />
-            <BooksViewSwitcher />
+            <div className="filter">
+                <BooksFilter />
+                <BooksViewSwitcher />
+            </div>
             {!isEmpty && <BooksList books={books} />}
             {isEmpty && <BooksEmpty />}
             {!isEmpty && <Pagination />}
@@ -25,6 +27,17 @@ function Books() {
 }
 
 const BooksStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 24px;
+
+    .filter {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px 0;
+    }
 `;
 
 export default Books;
