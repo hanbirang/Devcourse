b7b486749abb0fbaa94b3ba8f5a334e32dd7b87e
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 10:35:22 +0900

    [ w14d5 ] feat(Books): 도서 목록 페이지의 각 컴포넌트 적용

--- book-store-ref/book-store/src/pages/Books.tsx
+++ book-store-ref/book-store/src/pages/Books.tsx
@@ -1,15 +1,21 @@
 import { styled } from 'styled-components';
 import Title from '../components/common/Title';
+import BooksFilter from '../components/books/BooksFilter';
+import BooksList from '../components/books/BooksList';
+import BooksEmpty from '../components/books/BooksEmpty';
+import BooksViewSwitcher from '../components/books/BooksViewSwitcher';
+import Pagination from '../components/books/Pagination';
 
 function Books() {
    return (
     <>
     <Title size='large'>도서 검색 결과</Title>
         <BooksStyle>
-            {/* 필터 */}
-            {/* 목록 */}
-            {/* empty */}
-            {/* 페이지네이션 */}
+            <BooksFilter />
+            <BooksViewSwitcher />
+            <BooksList />
+            <BooksEmpty />
+            <Pagination />
         </BooksStyle>
     </>
    );
