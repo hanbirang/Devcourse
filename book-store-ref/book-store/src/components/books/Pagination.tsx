c209a79155767703eb78f1c817feb144625183de
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 10:33:21 +0900

    [ w14d5 ] feat(Pagination): Pagination 컴포넌트 생성

---
+++ book-store-ref/book-store/src/components/books/Pagination.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+function Pagination() {
+   return (
+    <PaginationStyle></PaginationStyle>
+   );
+}
+
+const PaginationStyle = styled.div`
+`;
+
+export default Pagination;
