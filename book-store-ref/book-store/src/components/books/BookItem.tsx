de4ddea5354713aa73154384bc659d4458feb0bd
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-04 10:18:22 +0900

    [ w14d5 ] feat(BookItem): BookItem 컴포넌트 생성

---
+++ book-store-ref/book-store/src/components/books/BookItem.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+function BookItem() {
+   return (
+    <BookItemStyle></BookItemStyle>
+   );
+}
+
+const BookItemStyle = styled.div`
+`;
+
+export default BookItem;
