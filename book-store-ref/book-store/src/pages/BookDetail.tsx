123768aa3d71a3cbe0f0a885c0200861b925d344
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-05 11:19:03 +0900

    [ w15d1 ] feat(BookDetail): 각 도서의 상세 정보 노출시키기

--- book-store-ref/book-store/src/pages/BookDetail.tsx
+++ book-store-ref/book-store/src/pages/BookDetail.tsx
@@ -1,6 +1,36 @@
 import { useParams } from 'react-router-dom';
 import { styled } from 'styled-components';
 import { useBook } from '../hooks/useBook';
+import { getImgSrc } from '../utils/image';
+import Title from '../components/common/Title';
+import { BookDetail as IBookDetail } from '../models/book.model';
+
+const bookInfoList = [
+   {
+      label: "카테고리",
+      key: "categoryName"
+   },
+   {
+      label: "포맷",
+      key: "form"
+   },
+   {
+      label: "페이지",
+      key: "pages"
+   },
+   {
+      label: "ISBN",
+      key: "isbn"
+   },
+   {
+      label: "출간일",
+      key: "pubDate"
+   },
+   {
+      label: "가격",
+      key: "price"
+   }
+];
 
 function BookDetail() {
    const bookId = useParams().id;
@@ -11,7 +41,25 @@ function BookDetail() {
 
    return (
       <BookDetailStyle>
-         {book.title}
+         <header className="header">
+            <div className="img">
+               <img src={getImgSrc(book.img)} alt={book.title} />
+            </div>
+         </header>
+         <div className="info">
+            <Title size='large' color='text'>{book.title}</Title>
+            {
+               bookInfoList.map((item) => (
+                  <dl>
+                     <dt>{item.label}</dt>
+                     <dd>{book[item.key as keyof IBookDetail]}</dd>
+                  </dl>
+               ))
+            }
+         </div>
+         <div className="content">
+
+         </div>
       </BookDetailStyle>
    );
 }
