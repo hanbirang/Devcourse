8712ca2b61b787adf3e911c63bf979196c88b0d4
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-05 10:03:38 +0900

    [ w15d1 ] feat(BookDetail): param의 bookId로 개별 도서 가져오기

--- book-store-ref/book-store/src/pages/BookDetail.tsx
+++ book-store-ref/book-store/src/pages/BookDetail.tsx
@@ -1,8 +1,18 @@
+import { useParams } from 'react-router-dom';
 import { styled } from 'styled-components';
+import { useBook } from '../hooks/useBook';
 
 function BookDetail() {
+   const bookId = useParams().id;
+   const { book } = useBook(bookId);
+
+   console.log(book);
+   if (!book) return null;
+
    return (
-    <BookDetailStyle></BookDetailStyle>
+      <BookDetailStyle>
+         {book.title}
+      </BookDetailStyle>
    );
 }
 
