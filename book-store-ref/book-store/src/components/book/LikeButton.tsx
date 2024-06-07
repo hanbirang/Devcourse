5af11f38a93c8fc3f8cdd41ff20944afe85cfb85
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 12:40:23 +0900

    [ w15d1 ] style(LikeButton): 좋아요 버튼 style 지정

--- book-store-ref/book-store/src/components/book/LikeButton.tsx
+++ book-store-ref/book-store/src/components/book/LikeButton.tsx
@@ -10,16 +10,22 @@ interface Props {
 
 function LikeButton({book, onClick}: Props) {
     return (
-        <LikeButtonStyle>
-            <Button size='medium' scheme='normal'>
-                <FaHeart />
-                {book.likes}
-            </Button>
+        <LikeButtonStyle size='medium' scheme='like'>
+            <FaHeart />
+            {book.likes}
         </LikeButtonStyle>
     );
-}
+};
 
-const LikeButtonStyle = styled.div`
+const LikeButtonStyle = styled(Button)`
+    display: flex;
+    gap: 6px;
+    svg{
+        color: inherit;
+        * {
+            color: inherit;
+        }
+    }
 `;
 
 export default LikeButton;
