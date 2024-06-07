a4cbd9f4d5a3e2d5d05be5f2db1513d4b689ee9a
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 15:58:52 +0900

    [ w15d1 ] feat(AddtoCart): 장바구니 담기 버튼을 누르면 문구가 나타났다가 사라지는 기능 추가

--- book-store-ref/book-store/src/components/book/AddtoCart.tsx
+++ book-store-ref/book-store/src/components/book/AddtoCart.tsx
@@ -3,12 +3,15 @@ import { BookDetail } from '../../models/book.model';
 import InputText from '../common/InputText';
 import Button from '../common/Button';
 import { useState } from 'react';
+import { addCart } from '../../api/carts.api';
+import { Link } from 'react-router-dom';
 
 interface Props {
     book: BookDetail;
 }
 function AddToCart({book}: Props) {
     const [quantity, setQuantity] = useState<number>(1);
+    const [cartAdded, setCartAdded] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setQuantity(Number(e.target.value));
@@ -23,8 +26,20 @@ function AddToCart({book}: Props) {
         setQuantity(quantity - 1);
     };
 
+    const addToCart = () => {
+        addCart({
+            book_id: book.id,
+            quantity: quantity
+        }).then(() => {
+            setCartAdded(true);
+            setTimeout(() => {
+                setCartAdded(false);
+            }, 3000);
+        });
+    }
+
     return (
-        <AddToCartStyle>
+        <AddToCartStyle $added={cartAdded}>
             <div>
                 <InputText 
                     inputType='number' value={quantity} 
@@ -37,17 +52,43 @@ function AddToCart({book}: Props) {
                     -
                 </Button>
             </div>
-            <Button size='medium' scheme='primary'>
+            <Button size='medium' scheme='primary' onClick={addToCart}>
                 장바구니 담기
             </Button>
+            <div className="added">
+                <p>장바구니에 추가되었습니다.</p>
+                <Link to='/cart'>장바구니로 이동</Link>
+            </div>
         </AddToCartStyle>
     );
-}
+};
 
-const AddToCartStyle = styled.div`
+interface AddToCartStyleProps {
+    $added: boolean;
+};
+
+const AddToCartStyle = styled.div<AddToCartStyleProps>`
     display: flex;
     justify-content: space-between;
     align-items: center;
+
+    position: relative;
+
+    .added {
+        position: absolute;
+        right: 0;
+        bottom: -90px;
+        background: ${({theme}) => theme.color.background};
+        border-radius: ${({theme}) => theme.borderRadius.default};
+        padding: 8px 12px;
+        opacity: ${({$added}) => ($added ? '1' : '0')};
+        transition: all 0.5s ease;
+
+        p {
+            padding: 0 0 8px 0;
+            margin: 0;
+        }
+    }
 `;
 
 export default AddToCart;
