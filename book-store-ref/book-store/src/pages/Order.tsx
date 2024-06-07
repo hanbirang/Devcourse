e77a6250b64c8981801ed26698613c8af9a09760
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-08 00:06:28 +0900

    [ w15d3 ] feat(Order): Order 페이지 컴포넌트 생성

--- book-store-ref/book-store/src/pages/Order.tsx
+++ book-store-ref/book-store/src/pages/Order.tsx
@@ -1,17 +1,71 @@
 import { useLocation } from 'react-router-dom';
-import { styled } from 'styled-components';
+import Title from '../components/common/Title';
+import { CartStyle } from './Cart';
+import CartSummary from '../components/cart/CartSummary';
+import Button from '../components/common/Button';
+import InputText from '../components/common/InputText';
 
 function Order() {
     const location = useLocation();
     const orderDataFromCart = location.state;
+    const { totalQuantity, totalPrice, firstBookTitle } = orderDataFromCart;
 
-    console.log(orderDataFromCart);
     return (
-        <OrderStyle></OrderStyle>
+    <>
+        <Title size='large'>주문서 작성</Title>
+        <CartStyle>
+            <div className="content">
+                <div className="order-info">
+                    <Title size='medium' color='text'>배송 정보</Title>
+                    <form className='delivery'>
+                        <fieldset>
+                            <label>주소</label>
+                            <div className="input">
+                                <InputText inputType='text' />
+                            </div>
+                            <Button size='medium' scheme='normal'>
+                                주소 찾기
+                            </Button>
+                        </fieldset>
+                        <fieldset>
+                            <label>상세 주소</label>
+                            <div className="input">
+                                <InputText inputType='text' />
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <label>수령인</label>
+                            <div className="input">
+                                <InputText inputType='text' />
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <label>전화번호</label>
+                            <div className="input">
+                                <InputText inputType='text' />
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+                <div className="order-info">
+                    <Title size='medium' color='text'>주문 상품</Title>
+                    <strong>
+                        {firstBookTitle} 등 총 {totalQuantity}권
+                    </strong>
+                </div>
+            </div>
+            <div className="summary">
+                <CartSummary 
+                    totalQuantity={totalQuantity} 
+                    totalPrice={totalPrice} 
+                />
+                <Button size='large' scheme='primary'>
+                    결제하기
+                </Button>
+            </div>
+        </CartStyle>
+    </>
     );
-}
-
-const OrderStyle = styled.div`
-`;
+};
 
 export default Order;
