e53ec79a53659da3614944d3e8ecff173842a036
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 17:30:13 +0900

    [ w15d2 ] feat(Cart): 장바구니 목록 페이지에 CartItem 추가

--- book-store-ref/book-store/src/pages/Cart.tsx
+++ book-store-ref/book-store/src/pages/Cart.tsx
@@ -1,14 +1,21 @@
 import { styled } from 'styled-components';
 import Title from '../components/common/Title';
 import CartItem from '../components/cart/CartItem';
+import { useCart } from '../hooks/useCart';
 
 function Cart() {
+    const { carts } = useCart();
+    console.log(carts);
     return (
         <>
             <Title size='large'>장바구니</Title>
             <CartStyle>
                 <div className="content">
-                    <CartItem />
+                    {
+                        carts.map((cart) => (
+                            <CartItem key={cart.id} cart={cart} />
+                        ))
+                    }
                 </div>
                 <div className="summary">
                     summary
