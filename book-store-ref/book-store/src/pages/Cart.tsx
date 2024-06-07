0b94fcc22bceb98d83e42fd931f2dd5f45f5025c
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 21:27:36 +0900

    [ w15d2 ] feat(Cart): 장바구니가 비었을 경우 빈 페이지 공통 컴포넌트를 이용하여 빈 페이지 노출

--- book-store-ref/book-store/src/pages/Cart.tsx
+++ book-store-ref/book-store/src/pages/Cart.tsx
@@ -3,9 +3,11 @@ import Title from '../components/common/Title';
 import CartItem from '../components/cart/CartItem';
 import { useCart } from '../hooks/useCart';
 import { useState } from 'react';
+import Empty from '../components/common/Empty';
+import { FaShoppingCart } from 'react-icons/fa';
 
 function Cart() {
-    const { carts, deleteCartItem } = useCart();
+    const { carts, isEmpty, deleteCartItem } = useCart();
     const [ checkedItems, setCheckedItems ] = useState<number[]>([]);
     const handleCheckItem = (id: number) => {
         if(checkedItems.includes(id)) {
@@ -28,21 +30,32 @@ function Cart() {
         <>
             <Title size='large'>장바구니</Title>
             <CartStyle>
-                <div className="content">
-                    {
-                        carts.map((cart) => (
-                            <CartItem 
-                                key={cart.id} cart={cart} 
-                                checkedItems={checkedItems}
-                                onCheck={handleCheckItem}
-                                onDelete={handleItemDelete}
-                            />
-                        ))
-                    }
-                </div>
-                <div className="summary">
-                    summary
-                </div>
+                {!isEmpty && (
+                    <>
+                        <div className="content">
+                            {
+                                carts.map((cart) => (
+                                    <CartItem 
+                                        key={cart.id} cart={cart} 
+                                        checkedItems={checkedItems}
+                                        onCheck={handleCheckItem}
+                                        onDelete={handleItemDelete}
+                                    />
+                                ))
+                            }
+                        </div>
+                        <div className="summary">
+                            summary
+                        </div>
+                    </>
+                )}
+                {isEmpty && (
+                    <Empty 
+                        icon={<FaShoppingCart />}
+                        title='장바구니가 비었습니다'
+                        description = {<>장바구니를 채워보세요.</>}
+                    />
+                )}
             </CartStyle>
         </>
     );
