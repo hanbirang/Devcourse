c5042844140dda4d1b669ef8412741eb8a946b5b
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 16:47:47 +0900

    [ w15d2 ] feat(CartItem): 장바구니 목록 아이템 틀 생성

---
+++ book-store-ref/book-store/src/components/cart/CartItem.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+function CartItem() {
+    return (
+        <CartItemStyle></CartItemStyle>
+    );
+}
+
+const CartItemStyle = styled.div`
+`;
+
+export default CartItem;
