ab06c710c2b6a0b3cf83a10e980e8100cde245d5
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 17:35:24 +0900

    [ w15d2 ] feat(CheckIconButton): 체크 버튼 컴포넌트 생성

---
+++ book-store-ref/book-store/src/components/cart/CheckIconButton.tsx
@@ -0,0 +1,24 @@
+import { FaRegCheckCircle, FaRegCircle } from 'react-icons/fa';
+import { styled } from 'styled-components';
+
+interface Props {
+    isChecked: boolean;
+    onCheck: () => void;
+}
+function CheckIconButton({isChecked, onCheck}: Props) {
+    return (
+        <CheckIconButtonStyle onClick={onCheck}>
+            {
+                isChecked ?
+                (<FaRegCheckCircle />) : (<FaRegCircle />)
+            }
+            <FaRegCircle />
+            <FaRegCheckCircle />
+        </CheckIconButtonStyle>
+    );
+}
+
+const CheckIconButtonStyle = styled.button`
+`;
+
+export default CheckIconButton;
