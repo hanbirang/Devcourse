d7a1626d9f94be6f0ea3b6186b73ba61f5070637
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 17:34:39 +0900

    [ w15d2 ] style(CartItem): 장바구니 목록 아이템 (CartItem) style 지정

--- book-store-ref/book-store/src/components/cart/CartItem.tsx
+++ book-store-ref/book-store/src/components/cart/CartItem.tsx
@@ -3,6 +3,7 @@ import { Cart } from '../../models/cart.model';
 import Button from '../common/Button';
 import Title from '../common/Title';
 import { formatNumber } from '../../utils/format';
+import CheckIconButton from './CheckIconButton';
 
 interface Props {
     cart: Cart;
@@ -12,9 +13,10 @@ function CartItem({cart}: Props) {
     return (
         <CartItemStyle>
             <div className="info">
+                <CheckIconButton />
                 <div>체크 버튼</div>
                 <div>
-                    <Title size='medium'>{cart.title}</Title>
+                    <Title size='medium' color='text'>{cart.title}</Title>
                     <p className="summary">{cart.summary}</p>
                     <p className="price">{formatNumber(cart.price)} 원</p>
                     <p className="quantity">{cart.quantity} 권</p>
@@ -28,6 +30,17 @@ function CartItem({cart}: Props) {
 }
 
 const CartItemStyle = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    border: 1px solid ${({theme}) => theme.color.border};
+    border-radius: ${({theme}) => theme.borderRadius.default};
+    padding: 12px;
+
+    p {
+        padding: 0 0 8px 0;
+        margin: 0;
+    }
 `;
 
 export default CartItem;
