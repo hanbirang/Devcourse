3d567596897a12f83de61d240af4f786ad154729
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 17:46:14 +0900

    [ w15d2 ] style(CheckIconButton): 체크 버튼 style 지정

--- book-store-ref/book-store/src/components/cart/CheckIconButton.tsx
+++ book-store-ref/book-store/src/components/cart/CheckIconButton.tsx
@@ -17,6 +17,14 @@ function CheckIconButton({isChecked, onCheck}: Props) {
 }
 
 const CheckIconButtonStyle = styled.button`
+    background: none;
+    border: 0;
+    cursor: pointer;
+
+    svg {
+        width: 24px;
+        height: 24px;
+    }
 `;
 
 export default CheckIconButton;
