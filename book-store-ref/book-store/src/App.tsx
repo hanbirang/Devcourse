457ca0e469d978278ad2b2e201f95eee876b5b30
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 16:48:18 +0900

    [ w15d2 ] feat(App): router에 장바구니 목록 경로 추가

--- book-store-ref/book-store/src/App.tsx
+++ book-store-ref/book-store/src/App.tsx
@@ -8,6 +8,7 @@ import ResetPassword from "./pages/ResetPassword";
 import Login from "./pages/Login";
 import Books from "./pages/Books";
 import BookDetail from "./pages/BookDetail";
+import Cart from "./pages/Cart";
 
 const router = createBrowserRouter([
   {
@@ -45,6 +46,12 @@ const router = createBrowserRouter([
     path: "/books/:id",
     element: (
       <Layout><BookDetail /></Layout>
+    )
+  },
+  {
+    path: "/carts",
+    element: (
+      <Layout><Cart /></Layout>
     )
   }
 ]);
