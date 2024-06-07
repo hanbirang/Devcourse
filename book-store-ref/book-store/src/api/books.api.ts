44060b6f0b4db0e37701cf8339cae264bd428bc7
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 14:22:41 +0900

    [ w15d1 ] feat(books): 좋아요 버튼을 누르는 api 추가

--- book-store-ref/book-store/src/api/books.api.ts
+++ book-store-ref/book-store/src/api/books.api.ts
@@ -31,4 +31,9 @@ export const fetchBooks = async (params: FetchBooksParams) => {
 export const fetchBook = async (bookId: string) => {
     const response = await httpClient.get<BookDetail>(`/books/${bookId}`);
     return response.data;
-}
+};
+
+export const likeBook = async (bookId: number) => {
+    const response = await httpClient.post(`/likes/${bookId}`);
+    return response.data;
+};
