8f0099d476766ce2d4409110c899fbfc7f10b61f
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 16:03:52 +0900

    [ w15d1 ] feat(useBook): useBook hooks에 장바구니에 넣는 기능 추가

--- book-store-ref/book-store/src/hooks/useBook.ts
+++ book-store-ref/book-store/src/hooks/useBook.ts
@@ -3,10 +3,12 @@ import { BookDetail } from "../models/book.model";
 import { fetchBook, likeBook, unlikeBook } from "../api/books.api";
 import { useAuthStore } from "../store/authStore";
 import { useAlert } from "./useAlert";
+import { addCart } from "../api/carts.api";
 
 export const useBook = (bookId: string | undefined) => {
     const  [ book, setBook ] = useState<BookDetail | null>(null);
     const  { isloggedIn } = useAuthStore();
+    const [cartAdded, setCartAdded] = useState(false);
     const showAlert = useAlert();
 
     const likeToggle = () => {
@@ -37,6 +39,20 @@ export const useBook = (bookId: string | undefined) => {
         }
     };
 
+    const addToCart = (quantity: number) => {
+        if (!book) return;
+
+        addCart({
+            book_id: book.id,
+            quantity: quantity
+        }).then(() => {
+            setCartAdded(true);
+            setTimeout(() => {
+                setCartAdded(false);
+            }, 3000);
+        });
+    }
+
     useEffect(() => {
         if (!bookId) return;
         
@@ -45,5 +61,5 @@ export const useBook = (bookId: string | undefined) => {
         })
     }, [bookId]);
 
-    return { book, likeToggle };
+    return { book, likeToggle, addToCart, cartAdded };
 };
