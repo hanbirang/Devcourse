82ce673920d90361f93af0d51c3d88740b02fb4b
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 16:50:13 +0900

    [ w15d2 ] feat(carts): 장바구니 API에 fetchCart 함수 생성

--- book-store-ref/book-store/src/api/carts.api.ts
+++ book-store-ref/book-store/src/api/carts.api.ts
@@ -1,3 +1,4 @@
+import { Cart } from "../models/cart.model";
 import { httpClient } from "./http";
 
 interface AddCartParams {
@@ -9,3 +10,8 @@ export const addCart = async(params: AddCartParams) => {
     const response = await httpClient.post('/carts', params);
     return response.data;
 };
+
+export const fetchCart = async () => {
+    const response = await httpClient.get<Cart[]>('/carts');
+    return response.data;
+}
