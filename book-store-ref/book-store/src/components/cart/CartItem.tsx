b89fdb9adc3dce88512df9b5a764f405d591e572
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 17:00:52 +0900

    [ w15d2 ] feat(CartItem): 장바구니 목록 아이템 render

--- book-store-ref/book-store/src/components/cart/CartItem.tsx
+++ book-store-ref/book-store/src/components/cart/CartItem.tsx
@@ -1,8 +1,29 @@
 import { styled } from 'styled-components';
+import { Cart } from '../../models/cart.model';
+import Button from '../common/Button';
+import Title from '../common/Title';
+import { formatNumber } from '../../utils/format';
 
-function CartItem() {
+interface Props {
+    cart: Cart;
+}
+
+function CartItem({cart}: Props) {
     return (
-        <CartItemStyle></CartItemStyle>
+        <CartItemStyle>
+            <div className="info">
+                <div>체크 버튼</div>
+                <div>
+                    <Title size='medium'>{cart.title}</Title>
+                    <p className="summary">{cart.summary}</p>
+                    <p className="price">{formatNumber(cart.price)} 원</p>
+                    <p className="quantity">{cart.quantity} 권</p>
+                </div>
+            </div>
+            <Button size='medium' scheme='normal'>
+                장바구니 삭제
+            </Button>
+        </CartItemStyle>
     );
 }
 
