18fbe31e41d2361cdd625d89512c5d8035e2b7d3
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 21:44:33 +0900

    [ w15d2 ] style(Cart): 장바구니 목록 style 지정

--- book-store-ref/book-store/src/pages/Cart.tsx
+++ book-store-ref/book-store/src/pages/Cart.tsx
@@ -62,6 +62,21 @@ function Cart() {
 }
 
 const CartStyle = styled.div`
+    display: flex;
+    gap: 24px;
+    justify-content: space-between;
+    padding: 24px 0 0 0;
+
+    .content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
+
+    .summary {
+        display: flex;
+    }
 `;
 
 export default Cart;
