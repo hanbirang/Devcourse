807019417c4d0c107d7a219a60f0069620151639
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 23:18:01 +0900

    [ w15d2 ] feat(Order): Order (주문) 페이지 생성

---
+++ book-store-ref/book-store/src/pages/Order.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+function Order() {
+    return (
+        <OrderStyle></OrderStyle>
+    );
+}
+
+const OrderStyle = styled.div`
+`;
+
+export default Order;
