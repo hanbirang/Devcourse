fa4ebf8f35992d4281293ac8000e135bcacc6793
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-07 23:31:40 +0900

    [ w15d2 ] feat(Order): 전달된 데이터 확인

--- book-store-ref/book-store/src/pages/Order.tsx
+++ book-store-ref/book-store/src/pages/Order.tsx
@@ -1,6 +1,11 @@
+import { useLocation } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 function Order() {
+    const location = useLocation();
+    const orderDataFromCart = location.state;
+
+    console.log(orderDataFromCart);
     return (
         <OrderStyle></OrderStyle>
     );
