003d7e3c1c72e2595b8c9486c57b5d22843bcb44
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-10 12:44:55 +0900

    [ w15d3 ] feat(useOrders): selectOrderItem hooks 생성
    자세히 버튼을 누르면 detail 정보를 추가하는 기능

--- book-store-ref/book-store/src/hooks/useOrders.ts
+++ book-store-ref/book-store/src/hooks/useOrders.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react"
-import { Order } from "../models/order.model";
+import { OrderListItem } from "../models/order.model";
 import { fetchOrder, fetchOrders } from "../api/order.api";
 
 export const useOrders = () => {
-    const [ orders, setOrders ] = useState<Order[]>([]);
+    const [ orders, setOrders ] = useState<OrderListItem[]>([]);
     const [ selectedItemId, setSelectedItemId ] = useState<number | null>(null);
 
     useEffect(() => {
@@ -13,8 +13,25 @@ export const useOrders = () => {
     }, []);
 
     const selectOrderItem = (orderId: number) => {
+        // 요청 방어
+        if (orders.filter((item) => item.id === orderId)[0].detail) {
+            setSelectedItemId(orderId);
+            return;
+        }
+        
         fetchOrder(orderId).then((orderDetail) => {
-            console.log('orderDetail', orderDetail);
+            setSelectedItemId(orderId);
+            setOrders(
+                orders.map((item) => {
+                    if (item.id === orderId) {
+                        return {
+                           ...item,
+                            detail: orderDetail,
+                        };
+                    }
+                    return item;
+                })
+            );
         });
     };
 
