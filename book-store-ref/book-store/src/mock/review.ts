aeb32ea8ff2319e902b03e699a86932c8de3408b
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-11 16:27:31 +0900

    [ w16d1 ] feat(review): Faker를 사용하여 리뷰 더미 데이터 생성
    - faker-js/faker 모듈을 사용하여 리뷰 데이터를 생성하는 로직 추가.
    - 8개의 더미 리뷰 데이터를 포함하는 mockReviewData 배열 생성.
    - 사용자 이름, 리뷰 내용, 생성일자, 점수 등의 더미 데이터를 faker를 사용하여 생성.
    - MSW 핸들러에서 mockReviewData를 반환하도록 수정.

--- book-store-ref/book-store/src/mock/review.ts
+++ book-store-ref/book-store/src/mock/review.ts
@@ -1,9 +1,17 @@
 import { BookReviewItem } from "@/models/book.model";
 import { http, HttpResponse } from "msw";
+import { fakerKO as faker } from "@faker-js/faker";
+
+const mockReviewData: BookReviewItem[] = Array.from({length: 8}).map((_, index) => ({
+    id: index,
+    userName: `${faker.person.lastName()}${faker.person.firstName()}`,
+    content: faker.lorem.paragraph(),
+    createdAt: faker.date.past().toISOString(),
+    score: faker.helpers.rangeToNumber({min: 1, max: 5})
+}));
 
 export const reviewsById = http.get("http://localhost:6250/reviews/:bookId", () => {
-    const data: BookReviewItem[] = [];
-    return HttpResponse.json(data, {
+    return HttpResponse.json (mockReviewData, {
         status: 200
     });
 });
