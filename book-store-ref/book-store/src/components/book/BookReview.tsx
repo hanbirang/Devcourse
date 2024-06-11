09cb2e985c560986a3cdd8013d728b1bf2b87fa9
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-11 16:51:32 +0900

    [ w16d1 ] feat(BookReview): Book Review에 Book Review Item 컴포넌트 배치

---
+++ book-store-ref/book-store/src/components/book/BookReview.tsx
@@ -0,0 +1,23 @@
+import { BookReviewItem as IBookReviewItem } from '@/models/book.model';
+import { styled } from 'styled-components';
+import BookReviewItem from './BookReviewItem';
+
+interface Props {
+    reviews: IBookReviewItem[];
+}
+function BookReview({ reviews }: Props) {
+    return (
+        <BookReviewStyle>
+            {
+                reviews.map((review) => (
+                    <BookReviewItem review={review}/>
+                ))
+            }
+        </BookReviewStyle>
+    );
+}
+
+const BookReviewStyle = styled.div`
+`;
+
+export default BookReview;
