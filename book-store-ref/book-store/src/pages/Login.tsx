429acf126a34470ab8716d00d1011217638ad427
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-11 10:02:49 +0900

    [ w15d5 ] refactor(Login): useAuth 훅을 import 하여 로그인 기능 구현 > 코드 리팩토링

--- book-store-ref/book-store/src/pages/Login.tsx
+++ book-store-ref/book-store/src/pages/Login.tsx
@@ -1,40 +1,27 @@
 import Title from '../components/common/Title';
 import InputText from '../components/common/InputText';
 import Button from '../components/common/Button';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useForm } from "react-hook-form";
-import { login } from '../api/auth.api';
-import { useAlert } from '../hooks/useAlert';
 import { SignupStyle } from './Signup';
-import { useAuthStore } from '../store/authStore';
+import { useAuth } from '@/hooks/useAuth';
 
-export interface SignupProps {
+export interface LoginProps {
     email: string;
     password: string;
 }
 
 function Login() {
-    const navigate = useNavigate();
-    const {showAlert} = useAlert();
-
-    const { isloggedIn, storeLogin, storeLogout } = useAuthStore();
+    const { userLogin } = useAuth();
     
     const { 
         register, 
         handleSubmit, 
         formState: { errors }
-    } = useForm<SignupProps>();
+    } = useForm<LoginProps>();
     
-    const onSubmit = (data: SignupProps) => {
-        login(data).then((res) => {
-            // 상태 변화 
-            storeLogin(res.token);
-            console.log(res.token);
-            showAlert("로그인 완료되었습니다.");
-            navigate('/');
-        }, (error) => {
-            showAlert('로그인이 실패했습니다.');
-        });
+    const onSubmit = (data: LoginProps) => {
+        userLogin(data);
     };
 
     return (
