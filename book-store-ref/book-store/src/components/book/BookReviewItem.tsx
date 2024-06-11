1aa94be6cf62e06b7c85f318d7d83241937df28e
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-11 17:27:58 +0900

    [ w16d1 ] style(BookReviewItem): BookReviewItem style 지정

--- book-store-ref/book-store/src/components/book/BookReviewItem.tsx
+++ book-store-ref/book-store/src/components/book/BookReviewItem.tsx
@@ -4,7 +4,8 @@ import { styled } from 'styled-components';
 
 interface Props {
     review: IBookReviewItem;
-}
+};
+
 function BookReviewItem({ review }: Props) {
     return (
         <BookReviewItemStyle>
@@ -25,6 +26,28 @@ function BookReviewItem({ review }: Props) {
 }
 
 const BookReviewItemStyle = styled.div`
-`;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+    padding: 12px;
+    border-radius: ${({theme}) => theme.borderRadius.default};
 
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-size: 0.875rem;
+        color: ${({theme}) => theme.color.secondary};
+        padding: 0;
+    }
+
+    .content {
+        p {
+            font-size: 1rem;
+            line-height: 1.5;
+            margin: 0;
+        }
+    }
+`;
 export default BookReviewItem;
