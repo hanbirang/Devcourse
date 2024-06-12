b55bb07010365e67114be0908e9c0246888e999c
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-12 10:00:35 +0900

    [ w16d2 ] style(Toast): Toast component style 지정

--- book-store-ref/book-store/src/components/common/toast/Toast.tsx
+++ book-store-ref/book-store/src/components/common/toast/Toast.tsx
@@ -21,6 +21,38 @@ function Toast({ id, message, type }: ToastItem) {
     );
 }
 
-const ToastStyle = styled.div``;
+const ToastStyle = styled.div`
+    background-color: ${({theme}) => theme.color.background};
+    padding: 12px;
+    border-radius: ${({theme}) => theme.borderRadius.default};
+
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    gap: 24px;
+
+    p {
+        color: ${({theme}) => theme.color.text};
+        line-height: 1;
+        margin: 0;
+        flex: 1;
+
+        display: flex;
+        align-items: end;
+        gap: 4px;
+    }
+
+    button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        padding: 0;
+        margin: 0;
+
+        svg {
+            transform: rotate(45deg);
+        }
+    }
+`;
 
 export default Toast;
