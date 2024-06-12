a7db1d79a2f1f876cd51f77becbeaa5b8961bfa4
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-12 09:55:16 +0900

    [ w16d2 ] feat(App): App에 Toast Container 컴포넌트 배치

--- book-store-ref/book-store/src/App.tsx
+++ book-store-ref/book-store/src/App.tsx
@@ -13,64 +13,66 @@ import Order from "./pages/Order";
 import OrderList from "./pages/OrderList";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "./api/queryClient";
+import ToastContainer from "./components/common/toast/ToastContainer";
 
 const routeList = [
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/books",
-    element: <Books />
-  },
-  {
-    path: "/signup",
-    element: <Signup />
-  },
-  {
-    path: "/reset",
-    element: <ResetPassword />
-  },
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/books/:id",
-    element: <BookDetail />
-  },
-  {
-    path: "/cart",
-    element: <Cart />
-  },
-  {
-    path: "/order",
-    element: <Order />
-  },
-  {
-    path: '/orderlist',
-    element: <OrderList />
-  }
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/books",
+        element: <Books />,
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    },
+    {
+        path: "/reset",
+        element: <ResetPassword />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/books/:id",
+        element: <BookDetail />,
+    },
+    {
+        path: "/cart",
+        element: <Cart />,
+    },
+    {
+        path: "/order",
+        element: <Order />,
+    },
+    {
+        path: "/orderlist",
+        element: <OrderList />,
+    },
 ];
 
 const router = createBrowserRouter(
-  routeList.map((item) => {
-    return {
-      ...item,
-      element: <Layout>{item.element}</Layout>,
-      errorElement: <Error />
-    };
-  })
+    routeList.map((item) => {
+        return {
+            ...item,
+            element: <Layout>{item.element}</Layout>,
+            errorElement: <Error />,
+        };
+    })
 );
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BookStoreThemeProvider>
-        <RouterProvider router={router} />
-      </BookStoreThemeProvider>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BookStoreThemeProvider>
+                <RouterProvider router={router} />
+                <ToastContainer />
+            </BookStoreThemeProvider>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
