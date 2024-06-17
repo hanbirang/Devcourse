e799c3889b05d4f0cd78cfb58767f915f963aaec
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-17 21:40:32 +0900

    [ w16d3 ] feat(Home): 메인 페이지에 MainReview 컴포넌트 배치

--- book-store-ref/book-store/src/pages/Home.tsx
+++ book-store-ref/book-store/src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import MainReview from "@/components/main/MainReview";
+import { useMain } from "@/hooks/useMain";
+import { styled } from "styled-components";
+
 function Home() {
+    const { reviews } = useMain();
     return (
-        <>
-            <div>home body</div>
-        </>
+        <HomeStyle>
+            {/* 베너 */}
+
+            {/* 베스트셀러 */}
+
+            {/* 신간 */}
+
+            {/* 리뷰 */}
+            <MainReview reviews={reviews} />
+        </HomeStyle>
     );
 }
+
+const HomeStyle = styled.div``;
 
 export default Home;
