7d14c2d553a34bdc58664a6461cee9f5f0232011
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-18 21:14:09 +0900

    [ w16d3 ] feat(Banner): Banner 컴포넌트에 슬라이드 기능 추가
    - 배너 컴포넌트에 현재 인덱스를 관리하는 상태 추가
    - useMemo를 사용하여 현재 인덱스에 따른 변환 값 계산
    - 이전 및 다음 버튼을 통해 배너 슬라이드를 제어하는 handlePrev 및 handleNext 함수 추가
    - 배너 목록을 슬라이드로 보여주기 위해 BannerContainerStyle에 transform 속성 적용

--- book-store-ref/book-store/src/components/common/banner/Banner.tsx
+++ book-store-ref/book-store/src/components/common/banner/Banner.tsx
@@ -1,21 +1,59 @@
 import { Banner as IBanner } from "@/models/banner.model";
 import { styled } from "styled-components";
 import BannerItem from "./BannerItem";
+import { useMemo, useState } from "react";
 
 interface Props {
     banners: IBanner[];
 }
 
 function Banner({ banners }: Props) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const transFormValue = useMemo(() => {
+        return currentIndex * -100;
+    }, [currentIndex]);
+
+    const handlePrev = () => {
+        if (currentIndex === 0) return;
+        setCurrentIndex(currentIndex - 1);
+    };
+    const handleNext = () => {
+        if (currentIndex === banners.length - 1) return;
+        setCurrentIndex(currentIndex + 1);
+    };
+
     return (
         <BannerStyle>
-            {banners.map((item, index) => (
-                <BannerItem banner={item} />
-            ))}
+            {/* 베너 그룹 */}
+            <BannerContainerStyle $transFormValue={transFormValue}>
+                {banners.map((item, index) => (
+                    <BannerItem banner={item} />
+                ))}
+            </BannerContainerStyle>
+
+            {/* 버튼 */}
+            <BannerButtonStyle>
+                <button onClick={handlePrev}>이전</button>
+                <button onClick={handleNext}>다음</button>
+            </BannerButtonStyle>
+
+            {/* 인디케이터 */}
         </BannerStyle>
     );
 }
 
 const BannerStyle = styled.div``;
+
+interface BannerContainerStyleProps {
+    $transFormValue: number;
+};
+
+const BannerContainerStyle = styled.div<BannerContainerStyleProps>`
+    display: flex;
+    transform: translateX(${(props) => props.$transFormValue}%);
+`;
+
+const BannerButtonStyle = styled.div``;
 
 export default Banner;
