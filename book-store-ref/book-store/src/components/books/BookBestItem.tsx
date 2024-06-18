f9b17aa18ccd81b4fe9fbcb2af59ea1a293020a8
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-18 16:37:16 +0900

    [ w16d3 ] style(BookBestItem): BookBestItem style 수정

--- book-store-ref/book-store/src/components/books/BookBestItem.tsx
+++ book-store-ref/book-store/src/components/books/BookBestItem.tsx
@@ -23,6 +23,14 @@ const BookBestItemStyle = styled.div`
         .likes {
             display: none;
         }
+
+        h2 {
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
     }
 
     position: relative;
