af54376fc2e13a92a0dbe5d889196776d29a82bc
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-18 16:28:38 +0900

    [ w16d3 ] feat(Home): Home 페이지에 MainBest 컴포넌트 추가

--- book-store-ref/book-store/src/pages/Home.tsx
+++ book-store-ref/book-store/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import Title from "@/components/common/Title";
+import MainBest from "@/components/main/MainBest";
 import MainNewBooks from "@/components/main/MainNewBooks";
 import MainReview from "@/components/main/MainReview";
 import { useMain } from "@/hooks/useMain";
 import { styled } from "styled-components";
 
 function Home() {
-    const { reviews, newBooks } = useMain();
+    const { reviews, newBooks, bestBooks } = useMain();
     return (
         <HomeStyle>
             {/* 베너 */}
@@ -13,6 +14,7 @@ function Home() {
             {/* 베스트셀러 */}
             <section className="section">
                 <Title size="large">베스트 셀러</Title>
+                <MainBest books={bestBooks} />
             </section>
 
             {/* 신간 */}
