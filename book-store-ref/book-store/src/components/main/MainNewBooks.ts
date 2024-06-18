7a293b10b60a62612a3e1b17b1ab3d12de31bc3d
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-18 11:21:55 +0900

    [ w16d3 ] feat(MainNewBooks): MainNewBooks 컴포넌트 생성
    - 최신 도서 목록을 렌더링하는 MainNewBooks 컴포넌트 추가
    - BookItem 컴포넌트를 사용하여 각 도서 아이템을 그리드 형식으로 표시

---
+++ book-store-ref/book-store/src/components/main/MainNewBooks.ts
@@ -0,0 +1,20 @@
+import { Book } from "@/models/book.model";
+import { styled } from "styled-components";
+import BookItem from "../books/BookItem";
+
+interface Props {
+    books: Book[];
+}
+function MainNewBooks({ books }: Props) {
+    return (
+        <MainNewBooksStyle>
+            {books.map((book) => (
+                <BookItem key={book.id} book={book} view="grid" />
+            ))}
+        </MainNewBooksStyle>
+    );
+}
+
+const MainNewBooksStyle = styled.div``;
+
+export default MainNewBooks;
