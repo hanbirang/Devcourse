7ab5c3eecbc4116d7c2f42802f3cc045047e3499
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-06-19 11:34:53 +0900

    [ w16d4 ] style(Layout): Layout 컴포넌트에 반응형 그리드 레이아웃 추가

--- book-store-ref/book-store/src/components/layout/Layout.tsx
+++ book-store-ref/book-store/src/components/layout/Layout.tsx
@@ -6,21 +6,25 @@ interface LayoutProps {
     children: React.ReactNode;
 }
 
-function Layout({children}: LayoutProps) {
+function Layout({ children }: LayoutProps) {
     return (
         <>
             <Header />
             <LayoutStyle>{children}</LayoutStyle>
             <Footer />
         </>
-    )
+    );
 }
 
 const LayoutStyle = styled.main`
     width: 100%;
     margin: 0 auto;
-    max-width: ${({ theme }) =>  theme.layout.width.large};
+    max-width: ${({ theme }) => theme.layout.width.large};
     padding: 20px 0;
+
+    @media screen AND ${({ theme }) => theme.mediaQuery.mobile} {
+        padding: 20px 12px;
+    }
 `;
 
 export default Layout;
